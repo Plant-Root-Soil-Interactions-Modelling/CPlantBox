f0bac037c6d920b7a50fc077a5c3b121a8f09a5e
xiaoranzhou <zxrzxr@gmail.com>
2018-04-04 11:12:02 +0200

    new Jupyter Notebook and vtp reader
    Former-commit-id: 80fe1767de579dd01726a75e10217ec95ac35b22

--- results/vtpreader.py
+++ results/vtpreader.py
@@ -1,18 +1,18 @@
 import vtk
 
 reader = vtk.vtkXMLPolyDataReader()
-path = "example_5a.vtp" #Archive path
+path = "example_5a.vtp" #path or name of the vtp output
 reader.SetFileName(path)
 reader.Update()
  
 mapper = vtk.vtkPolyDataMapper()
 mapper.SetInputData(reader.GetOutput())
  
-coneActor = vtk.vtkActor()
-coneActor.SetMapper( mapper )
+plantActor = vtk.vtkActor()
+plantActor.SetMapper( mapper )
 
 ren1= vtk.vtkRenderer()
-ren1.AddActor( coneActor )
+ren1.AddActor( plantActor )
 ren1.SetBackground( 0.1, 0.2, 0.4 )
 
 renWin = vtk.vtkRenderWindow()
