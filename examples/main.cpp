b7eeaabc00ad0c0695e4e2ab784c69c5d054f8bf
xiaoranzhou <zxrzxr@gmail.com>
2018-07-06 09:27:57 +0200

    cmake update
    Former-commit-id: 35c5d0cef7cfdf16fd32a7eb5e81cebe67667307

---
+++ examples/main.cpp
@@ -0,0 +1,42 @@
+// Copyright (C) 2016 Daniel Leitner and Andrea Schnepf. See //license.txt for details.
+///This is an under developed version with a simple stem growth.
+#include <istream>
+#include "Plant.h"
+#include "Organ.h"
+#include "analysis.h"
+
+#include <iostream>
+#include <fstream>
+//#include <unistd.h>
+#include "tinyxml2.h"
+
+
+#include "example1.h"
+#include "example2.h"
+#include "example3.h"
+#include "example_scenegraph.h"
+
+
+/**
+ * Starts an examples (from the examples folder)
+ */
+int main(int argc, char* argv[])
+{
+	string name = "CPlantBox_test_Xiaoran";
+
+	if (argc>1) {
+		name= argv[1];
+	}
+
+	example1(); // open parameter file, and output VTP
+	// example2(); // like example 1, but with put geometry
+	// example3(); // more than 1 plant
+
+	// example_scenegraph();
+
+	return(0);
+
+}
+
+
+
