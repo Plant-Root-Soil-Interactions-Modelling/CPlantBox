f7d6d32438ce3d601638b3898334b451cc0b0860
xiaoran zhou <zxrzxr@gmail.com>
2018-07-30 14:18:38 -0700

    update
    Former-commit-id: 337ed263ed4c1ad0b87e4b42a380131b5dec0cd1

--- python/cmake_install.cmake
+++ python/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/crootbox/CPlantBox/python
+# Install script for directory: /home/crootbox/cpb/CPlantBox/python
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
