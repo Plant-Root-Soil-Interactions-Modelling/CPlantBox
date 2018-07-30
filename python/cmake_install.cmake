eaaa84808e130fc25901a08de524f50d74885f04
xiaoran zhou <zxrzxr@gmail.com>
2018-07-30 11:19:52 -0700

    update
    Former-commit-id: f1e9ac35937491561994ce5b1a7b2cf5154292cb

--- python/cmake_install.cmake
+++ python/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/x.zhou/CPlantBox/python
+# Install script for directory: /home/crootbox/CPlantBox/python
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -30,5 +30,10 @@ endif()
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
