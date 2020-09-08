215db19e53f3a1be7024c3bbc12b1392b51690fd
Daniel Leitner <daniel.leitner@simwerk.at>
2020-09-08 16:55:58 +0200

    general analysis started

--- applications/parametrisation/faba_taproot.py
+++ applications/parametrisation/faba_taproot.py
@@ -14,13 +14,15 @@ times = [7, 11, 15]  # measurement times (not in the rsml)
 names = ["Faba_12/DAP15.rsml", "Faba_14/DAP15.rsml", "Faba_16/DAP15.rsml", "Faba_20/DAP15.rsml", "Faba_24/DAP15.rsml" ]
 
 """ open all files, and split into measurent times"""
-polylines = [ [ [] for t in times] for i in range(0, len(names)) ] 
-properties = [ [ [] for t in times] for i in range(0, len(names)) ] 
-functions = [ [ [] for t in times] for i in range(0, len(names)) ]    
+polylines = [ [ [] for t in times] for i in range(0, len(names)) ]
+properties = [ [ [] for t in times] for i in range(0, len(names)) ]
+functions = [ [ [] for t in times] for i in range(0, len(names)) ]
 for i in range(0, len(names)):
-    print(names[i])   
-    j = len(times) - 1     
+    print(names[i])
+    j = len(times) - 1
     p, properties[i][j], functions[i][j] = rsml.read_rsml("RSML/" + names[i])  # read file to final time step
+    print(properties[i][j].keys())
+    print(functions[i][j].keys())
     p = [[np.array([p[i][j][k] / 10 for k in range(0, 3)]) for j in range(0, len(p[i])) ] for i in range(0, len(p)) ]  # mm -> cm
     polylines[i][j] = p
     for k in range(0, j):  # truncate the others
@@ -28,8 +30,8 @@ for i in range(0, len(names)):
 # polylines[i][j] contains i-th plant, j-th measurement time
 
 """ find all base roots """
-base_polylines = [ [ [] for t in times] for i in range(0, len(names)) ] 
-base_properties = [ [ [] for t in times] for i in range(0, len(names)) ] 
+base_polylines = [ [ [] for t in times] for i in range(0, len(names)) ]
+base_properties = [ [ [] for t in times] for i in range(0, len(names)) ]
 for i in range(0, len(names)):
     for j in range(0, len(times)):
             base_polylines[i][j], base_properties[i][j] = es.base_roots(polylines[i][j], properties[i][j])
@@ -39,7 +41,7 @@ for i in range(0, len(names)):
 """ recalculate length (should not alter anything)"""
 for i in range(0, len(names)):
     for j in range(0, len(times)):
-            es.create_length(base_polylines[i][j], base_properties[i][j]) 
+            es.create_length(base_polylines[i][j], base_properties[i][j])
 
 """ fit data """
 print()
@@ -63,19 +65,19 @@ y1 = es.negexp_length(t_, r1, k1)
 y2 = es.negexp_length(t_, r2, k2)
 plt.plot([0.], [0.], "r*")  # we can add that point
 for i in range(0, len(names)):
-    for j in range(0, len(times)):   
+    for j in range(0, len(times)):
         l = base_properties[i][j]["length"]
         plt.plot([times[j] for k in l], l, "k*")
-plt.plot(t_, y0, "b", label="first only, k fixed")
-plt.plot(t_, y1, "g", label="k fixed")
-plt.plot(t_, y2, "r", label="fit r, k")
+plt.plot(t_, y0, "b", label = "first only, k fixed")
+plt.plot(t_, y1, "g", label = "k fixed")
+plt.plot(t_, y2, "r", label = "fit r, k")
 
 plt.legend()
 plt.title("Faba")
 
 plt.xlabel("Time [days]")
 plt.ylabel("Length [cm]")
-plt.show()  
+plt.show()
 
 print("\nfin.")
 
