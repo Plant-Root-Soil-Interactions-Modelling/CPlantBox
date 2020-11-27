269a3bcf5152b7980a660991d8b1f724296afe24
Daniel Leitner <daniel.leitner@univie.ac.at>
2020-11-27 12:27:12 +0100

    fixed value to listbug

--- src/python_modules/xylem_flux.py
+++ src/python_modules/xylem_flux.py
@@ -42,7 +42,7 @@ class XylemFluxPython(XylemFlux):
             @return [cm] root xylem pressure per root system node         
          """
         # start = timeit.default_timer()
-        if isinstance(value, float):
+        if isinstance(value, (float,int)):
             n = len(self.seg_ind)
             value = [value/n]*n
             
@@ -68,7 +68,7 @@ class XylemFluxPython(XylemFlux):
                                       conductivity at the root surface will be limited by the value, i.e. kr = min(kr_root, k_soil)  
             @return [cm] root xylem pressure per root system node
          """
-        if isinstance(value, float):
+        if isinstance(value, (float,int)):
             n = len(self.node_ind)
             value = [value]*n       
          
