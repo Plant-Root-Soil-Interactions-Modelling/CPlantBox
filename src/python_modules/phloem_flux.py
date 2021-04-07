170d059798facf7e0cf2a2d3223cc0b948b3add9
m-giraud <m.giraud@fz-juelich.de>
2021-04-07 16:39:12 +0200

    phloem growth sink

--- src/python_modules/phloem_flux.py
+++ src/python_modules/phloem_flux.py
@@ -37,6 +37,18 @@ class PhloemFluxPython(Leuning):
         self.osmoCoeff = 1 #so for correspond to R*T * mean_phi
         self.D = 1
         self.exteriorCoeff =1.
+        self.Rm = 0.
+        self.GrSink = 0. # C used for Rg and growth
+        self.k1 = 0.0001
+        self.k2 = 0.01
+        self.Gr = 0.05 #C-limited growth during time step
+        self.GrEff = 0.5 #Growth efficiency ( = growth per unit of phi used)
+        self.k_growth = 0.001
+        self.phiThrGrowth = 0.001
+        self.lengthThr = 20 #max length
+        self.rhoC = 1 # C content per unit of volume
+        self.volumes = 0. #volume of each cell/segment
+        self.RmMax = 0.
 
     def mp2mesh(self, segs): #create an old2newNodeID map?
         """ Converts a mappedPlant into a network of 1D grids            
@@ -51,6 +63,8 @@ class PhloemFluxPython(Leuning):
         facesNorm = {} #map face to normal vector
         organTypes = self.get_organ_types()
         nOld = nodes[0]
+        radii = np.array([])
+        length = np.array([])
         for seg in segs:
             seg = np.array(list(map(lambda x: np.array(x), seg))) # converts the list of Vector3d to a numpy array. shape: (num_nodes, 3)
             for j, n in enumerate(seg):
@@ -64,6 +78,8 @@ class PhloemFluxPython(Leuning):
                 normal = (nOld - n)/norm(nOld - n)
                 #print('normal ', nOld, n, norm(nOld - n), normal)
                 if j > 0: #not first node of organ
+                    radii = np.concatenate((radii, [self.rs.radii[oldNodeID - 1]]))
+                    length = np.concatenate( (length, [norm(nOld - n)]))
                     if cells.size == 0 : #first organ of the plant
                         cells =  np.array((np.array([newNodeID - 1]),np.array([newNodeID]),np.array([-1]))) #nodes should be ordered. so seg n goes between nodes j-1 and j
                         facesNorm[newNodeID] = normal
@@ -102,52 +118,63 @@ class PhloemFluxPython(Leuning):
                 vertices = nodes_coord
             else:
                 vertices = np.hstack((vertices, nodes_coord))
-        
         self.faces = np.array((np.arange(0, vertices[0].size),), dtype=np.int64) 
         cells = MA.masked_values(cells, -1)
-        self.mesh = Mesh1Dmod(facesNorm=facesNorm, vertexCoords=vertices, faceVertexIDs=self.faces, cellFaceIDs=cells)
+        self.mesh = Mesh1Dmod(facesNorm=facesNorm,radii = radii,length = length, vertexCoords=vertices, faceVertexIDs=self.faces, cellFaceIDs=cells)
+        self.volumes = np.array([(np.pi * self.mesh.radii[xi]**2) *self.mesh.length[xi] for xi in range(self.mesh.numberOfCells) ])
         self.D = FaceVariable(name = 'diffusion coefficient', mesh = self.mesh, value = self.osmoCoeff)
         self.Source = CellVariable(name = 'An', mesh = self.mesh, value = 0.)
+        self.Rm = CellVariable(name = 'Rm', mesh = self.mesh, value = 0.)
+        self.GrSink = CellVariable(name = 'GrSink', mesh = self.mesh, value = 0.)
+        self.GrC = CellVariable(name = 'GrC', mesh = self.mesh, value = 0.)
+        self.GrEff= CellVariable(name = 'GrEff', mesh = self.mesh, value = 0.5)
         self.exteriorCoeff = FaceVariable(self.mesh,value=0.,rank=1)
         self.phi = CellVariable(name="solution variable", mesh=self.mesh,value = 0., hasOld = True)
         self.setD() #no diffusion to/from tips 
         #self._setFreeOutflow() #=>  free flow BCs for root-tips
         self.setAnSource() #to reset each time we have new An value
+        self.setRmSink()
+        self.setGrSink()
         
-    def _setFreeOutflow(self): #or set dirichlet of 0?
-        self.exteriorCoeff.setValue(0.) #reset for security 
+    def _setFreeOutflow(self): #or set dirichlet of 0 on outer face? or fixed gradient?
+        #self.exteriorCoeff.setValue(0.) #reset for security 
         tiproots, tipstem, tipleaf = self.get_organ_nodes_tips()
         tiproots_newID = np.array([self.old2newNodeID[xi] for xi in tiproots]).flatten()
         tiproots=  [np.any([tiproots_newID == yi]) for yi in self.mesh.faceVertexIDs[0]]
-        self.exteriorCoeff.constrain(value= self.osmoCoeff * self.phi.faceGrad, where=tiproots) #t change when flow defined in another way
-        print('_setFreeOutflow::extcoef ',self.exteriorCoeff)
+        print(len(tiproots), np.where(tiproots))
+        self.phi.constrain(0., where = tiproots)
 
     def setAnSource(self): #sourceterm in leaf segment
         self.Source.setValue(0.) #= CellVariable(name = 'An', mesh = self.mesh, value = 0.) #reset for security
         # find seg index with leaf, should be ordered like the An vector
         nodeYLeaf = self.get_segments_index(4) + 1 #ynode_index of leaves
-        print('setAnSource::nodeYLeaf ', nodeYLeaf)        
         nodeYLeaf_newID = np.array([self.old2newNodeID[xi] for xi in nodeYLeaf]).flatten() #new index
-        print('setAnSource::nodeYLeaf_newID ',nodeYLeaf_newID)        
-        #self.D.constrain(0., where = leaves) no need? default face constrain is no flux
         for i, an in enumerate(self.An):
                 self.Source.constrain(an, where= self.mesh.cellFaceIDs[0] == nodeYLeaf_newID[i]) #0 node = y node for stem and leaves
-        print('setAnSource::Source ',self.Source, len(self.Source))
 
     def setD(self):
         self.D.setValue(self.osmoCoeff) 
+        tiproots, tipstem, tipleaf = self.get_organ_nodes_tips()
+        #tips = np.concatenate((tipstem, tipleaf)).flatten()
         tips = np.concatenate((self.get_organ_nodes_tips())).flatten()
-        print('_setNoDiffBCs::tips ', tips)
         tips_newID = np.array([self.old2newNodeID[xi] for xi in tips]).flatten()
-        print('_setNoDiffBCs::tips_newID ', tips_newID)
         tips=  np.array(([np.any([tips_newID == yi]) for yi in self.mesh.faceVertexIDs[0]])).flatten()
-        print('_setNoDiffBCs::tips ', tips)
-        self.D.constrain(0., where = tips)
-        print('_setNoDiffBCs::D ', self.D)
-        self._setFreeOutflow() #update freeoutflowBC very time we update D coeff
+        self.D.constrain(0., where = tips) 
       
-    def setRmSink(self): 
-        return 0. #TODO
+    def setRmSink(self): # Daudet 2002: Rm = (k1 + k2 * C)Sr. set k1 = 0
+        self.RmMax = self.rhoC * self.volumes * (self.phi * self.k1 + self.k2 )#  * (self.phi - self.phiThrGrowth) error when use a k2
+        self.CSat = self.RmMax < self.phi*0.9 #need to add 0.9 factor or phi goes < 0
+        self.Rm = (self.RmMax * (self.RmMax < self.phi*0.9)  + 0.9*self.phi* (self.RmMax >= 0.9*self.phi)) 
         
-    def setRgSink(self): 
-        return 0. #TODO
+    def setGrSink(self): #Rg + C allocation = Grsink = growth * (1/GrowEff)
+        self.Gr = self.k_growth  * self.mesh.length * (-self.mesh.length + self.lengthThr) * (self.phi - self.phiThrGrowth) * (self.mesh.length < self.lengthThr)* (self.phi > self.phiThrGrowth) #adapted from Daudet volumic growth for next time step. To send to CPlantBox
+        #extra length therm: then logarithmic instead of plateau
+        self.GrSink =self.Gr * self.rhoC * (1/self.GrEff)#growth with C content
+        
+        
+    def updateSourceSink(self): #usefull? will have to create a mesh at each timestep.
+        self.setAnSource()
+        self.setD()
+        self.setRmSink()
+        self.setGrSink()
+        return 0.
