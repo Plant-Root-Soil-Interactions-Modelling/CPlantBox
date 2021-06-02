505665b9f11a567a21c6e5b643288dc49569bb92
m-giraud <71639737+m-giraud@users.noreply.github.com>
2021-06-02 20:06:17 +0200

    Delete exampletest.py

--- tutorial/examples/python/exampletest.py
+++
@@ -1,8 +0,0 @@
-"""small example"""
-import sys
-sys.path.append("../../.."); sys.path.append("../../../src/python_modules"); sys.path.append("../../../test")
-import plantbox as pb
-import vtk_plot as vp
-import test_root as tr
-rs = tr.TestRoot()
-rs.root_dxMin_test([1., 2., 10.])
