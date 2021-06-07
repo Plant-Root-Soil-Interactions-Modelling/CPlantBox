c0ef790e1d8402984633f27c51c03a1950456b63
DanielLeitner <daniel.leitner@simwerk.at>
2021-06-07 09:24:40 +0200

    src/python_modules/vtk_plot.py

--- src/python_modules/vtk_plot.py
+++ src/python_modules/vtk_plot.py
@@ -11,6 +11,66 @@ VTK Plot, by Daniel Leitner (refurbished 06/2020)
 to make interactive vtk plot of root systems and soil grids
 """
 
+"""
+creates a leaf as vtkPolygon
+"""
+
+
+def create_leaf():
+    pass
+
+# int main(int, char*[])
+# {
+#   vtkNew<vtkNamedColors> colors;
+# 
+#   // Setup four points
+#   vtkNew<vtkPoints> points;
+#   points->InsertNextPoint(0.0, 0.0, 0.0);
+#   points->InsertNextPoint(1.0, 0.0, 0.0);
+#   points->InsertNextPoint(1.0, 1.0, 0.0);
+#   points->InsertNextPoint(0.0, 1.0, 0.0);
+# 
+#   // Create the polygon
+#   vtkNew<vtkPolygon> polygon;
+#   polygon->GetPointIds()->SetNumberOfIds(4); // make a quad
+#   polygon->GetPointIds()->SetId(0, 0);
+#   polygon->GetPointIds()->SetId(1, 1);
+#   polygon->GetPointIds()->SetId(2, 2);
+#   polygon->GetPointIds()->SetId(3, 3);
+# 
+#   // Add the polygon to a list of polygons
+#   vtkNew<vtkCellArray> polygons;
+#   polygons->InsertNextCell(polygon);
+# 
+#   // Create a PolyData
+#   vtkNew<vtkPolyData> polygonPolyData;
+#   polygonPolyData->SetPoints(points);
+#   polygonPolyData->SetPolys(polygons);
+# 
+#   // Create a mapper and actor
+#   vtkNew<vtkPolyDataMapper> mapper;
+#   mapper->SetInputData(polygonPolyData);
+# 
+#   vtkNew<vtkActor> actor;
+#   actor->SetMapper(mapper);
+#   actor->GetProperty()->SetColor(colors->GetColor3d("Silver").GetData());
+# 
+#   // Visualize
+#   vtkNew<vtkRenderer> renderer;
+#   vtkNew<vtkRenderWindow> renderWindow;
+#   renderWindow->SetWindowName("Polygon");
+#   renderWindow->AddRenderer(renderer);
+#   vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+#   renderWindowInteractor->SetRenderWindow(renderWindow);
+# 
+#   renderer->AddActor(actor);
+#   renderer->SetBackground(colors->GetColor3d("Salmon").GetData());
+#   renderWindow->Render();
+#   renderWindowInteractor->Start();
+# 
+#   return EXIT_SUCCESS;
+# }
+
 
 def solver_to_polydata(solver, min_, max_, res_):
     """ Creates vtkPolydata from dumux-rosi solver as a structured grid
@@ -28,7 +88,7 @@ def solver_to_polydata(solver, min_, max_, res_):
     return pd
 
 
-def segs_to_polydata(rs, zoom_factor=1., param_names=["age", "radius", "type", "creationTime"]):
+def segs_to_polydata(rs, zoom_factor=1., param_names=["age", "radius", "type", "organType" "creationTime"]):
     """ Creates vtkPolydata from a RootSystem or Plant using vtkLines to represent the root segments 
     @param rs             a RootSystem, Plant, or SegmentAnalyser
     @param zoom_factor    a radial zoom factor, since root are sometimes too thin for vizualisation
@@ -288,14 +348,14 @@ def plot_roots(pd, p_name:str, win_title:str="", render:bool=True):
     @return a tuple of a vtkActor and the corresponding color bar vtkScalarBarActor
     """
     if isinstance(pd, pb.RootSystem):
-        pd = segs_to_polydata(pd, 1., [p_name, "radius"])
+        pd = segs_to_polydata(pd, 1., [p_name, "radius", "organType"])
+
+    if isinstance(pd, pb.Plant):
+        pd = segs_to_polydata(pd, 1., [p_name, "radius", "organType"])
 
     if isinstance(pd, pb.SegmentAnalyser):
-        pd = segs_to_polydata(pd, 1., [p_name, "radius"])
+        pd = segs_to_polydata(pd, 1., [p_name, "radius", "organType"])
 
-    if isinstance(pd, pb.MappedPlant):
-        pd = segs_to_polydata(pd, 1., [p_name, "radius"])
-        
     if win_title == "":
         win_title = p_name
 
