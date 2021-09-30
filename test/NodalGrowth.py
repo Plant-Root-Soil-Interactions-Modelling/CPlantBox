b7e4e4b4a2425a94caf5ce08d5174e6875d70adf
m-giraud <71639737+m-giraud@users.noreply.github.com>
2021-09-30 15:29:37 +0200

    Update and rename example10d_22ss.py to NodalGrowth.py

--- test/NodalGrowth.py
+++ test/NodalGrowth.py
@@ -1,31 +1,25 @@
 
 
-import sys; sys.path.append(".."); sys.path.append("../src/python_modules");sys.path.append("../src/cython_modules")
-sys.path.append("/home/rbtlm2004/CPlantBoxinline/src");sys.path.append("/home/rbtlm2004/CPlantBoxinline/src/python_modules")
+import sys; sys.path.append(".."); sys.path.append("../src/python_modules");#sys.path.append("../src/cython_modules")
+#sys.path.append("/home/rbtlm2004/CPlantBoxinline/src");sys.path.append("/home/rbtlm2004/CPlantBoxinline/src/python_modules")
 #sys.path.append("/home/rbtlm2004/miniconda3/envs/MYFIPYENV/lib/python3.8/site-packages/fipy_changed")
 
 import plantbox as pb
 import vtk_plot as vp
-#from phloem_flux import PhloemFluxPython, GetTime
 import math
 import os
 import pstats
 from io import StringIO
-from datetime import datetime, timedelta
 import numpy as np
 import random
-       
-class NullIO(StringIO):
-    def write(self, txt):
-       pass
 
 test = os.listdir(os.getcwd())       
 for item in test:
-    if item.endswith("10b.txt"):
+    if item.endswith("NodalGrowth.txt"):
         os.remove(os.path.join(item))
-    if item.endswith("10b.vtk"):
+    if item.endswith("NodalGrowth.vtk"):
         os.remove(os.path.join( item))
-    if item.endswith("10b.vtp"):
+    if item.endswith("NodalGrowth.vtp"):
         os.remove(os.path.join(item))
 
     
@@ -36,10 +30,10 @@ for item in test:
 #
 ####################### 
 #random.seed(10)
-pl = pb.Plant(seednum = 1)
+pl = pb.MappedPlant(seednum = 1)
 #take care of MappedPlant afterwards
 path = "../modelparameter/plant/" #"../../../modelparameter/rootsystem/" 
-name = "4testrel"# "Triticum_aestivum_adapted_2021"# "Triticum_aestivum_adapted_2021"#"22ss"#"small_2020"
+name = "4testrel"
 pl.readParameters(path + name + ".xml")
 dt =1
 steps =35
@@ -55,5 +49,5 @@ for step in range(steps):
         
      
     ana = pb.SegmentAnalyser(pl)
-    ana.write("%s_example10b.vtp" %(step))
+    ana.write("%s_NodalGrowth.vtp" %(step))
 
