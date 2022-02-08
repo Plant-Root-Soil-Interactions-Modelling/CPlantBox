94e86c95fdd89c75a538f0cfb5194bd4ce8e3c60
m-giraud <71639737+m-giraud@users.noreply.github.com>
2022-02-08 17:03:51 +0100

    Update test_leafparameter.py

--- test/test_leafparameter.py
+++ test/test_leafparameter.py
@@ -130,7 +130,7 @@ class TestLeafParameter(unittest.TestCase):
         phi = np.array([-90., -67.5, -45, -22.5, 0, 22.5, 45, 67.5, 90]) / 180. * np.pi
         l = np.array([5., 1, 5, 1, 5, 1, 5, 1, 5])
         assert(l.shape == phi.shape)
-        N = 10#500  # N is rather high for testing
+        N = 500  # N is rather high for testing
         lrp.createLeafRadialGeometry(phi, l, N)          
            
         # self.assertEqual(lrp.leafMid(), 3., "unexpected leaf mid")
