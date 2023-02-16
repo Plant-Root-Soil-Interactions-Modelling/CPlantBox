7dfc1a4df066e7cdbc257e11602cf6b53f3e68b2
Daniel Leitner <daniel.leitner@simwerk.at>
2023-02-16 15:27:33 +0100

    updates

--- src/python_modules/HydraulicsDoussan.py
+++ src/python_modules/HydraulicsDoussan.py
@@ -36,17 +36,16 @@ class HydraulicsDoussan(XylemFluxPython):
             vv_.append(1.)
         return sparse.coo_matrix((np.array(vv_), (np.array(ii_), np.array(jj_))), shape = (sn, nn))
 
-    def doussan_system(self, sim_time, sxx, cells = True, soil_k = []):
-        """ sets up the system matrix self.A and load vector"self.b """
+    def doussan_system_matrix(self, sim_time):
+        """ """
         IM = self.get_incidence_matrix()
         IMt = IM.transpose()
         kx_ = np.divide(self.getKx(sim_time), self.rs.segLength())  # / dl
         Kx = sparse.diags(kx_)
-        kr = np.zeros((IM.shape[1],))
-        kr[1:] = np.array(self.getEffKr(sim_time))  #  change to getKr(), use that instead
+        kr = np.array(self.getEffKr(sim_time))
         Kr = sparse.diags(kr)
-        A = IMt @ Kx @ IM + Kr  # Laplacian IMt @ Kx @ IM =: L in Hess paper
-        return A
+        L = IMt @ Kx @ IM  # Laplacian
+        return (L[1:, 1:]).tocsc() + Kr, Kr, Kx  # L_{N-1} + Kr, se Hess paper
 
         # if cells:
         #     hs = np.zeros((IM.shape[1],))
@@ -100,6 +99,9 @@ class HydraulicsDoussan(XylemFluxPython):
         """
         s = self.rs.segments[seg_ind]
         i, j = s.x, s.y
+        n1, n2 = self.rs.nodes[i], self.rs.nodes[j]  # nodes
+        v = n2.minus(n1)
+        l = v.length()
         a = self.rs.radii[seg_ind]  # radius
         st = int(self.rs.subTypes[seg_ind])  # sub type
         age = sim_time - self.rs.nodeCTs[int(s.y)]
@@ -108,4 +110,33 @@ class HydraulicsDoussan(XylemFluxPython):
         dpdz0 = (rx[j] - rx[i]) / l
         f = -kx * (dpdz0)
         return f
+
+    def collar_flux(self, sim_time, rx):
+        """ returns the exact transpirational flux of the xylem model solution @param rx
+            @see axial_flux        
+        """
+        return self.axial_flux(0, sim_time, rx)
+
+    def axial_fluxes(self, sim_time, rx):
+        """ returns the axial fluxes 
+        @see axial_flux  
+        """
+        n = len(self.rs.segments)  # TODO getter
+        return np.array([self.axial_flux(i, sim_time, rxs) for i in range(0, n)])
+
+    def get_krs(self, sim_time, seg_ind = [0]):
+        """ calculatets root system conductivity [cm2/day] at simulation time @param sim_time [day] 
+        if there is no single collar segment at index 0, pass indices using @param seg_ind, see find_base_segments        
+        """
+        segs = self.rs.segments
+        nodes = self.rs.nodes
+        p_s = np.zeros((len(segs),))
+        for i, s in enumerate(segs):
+            p_s[i] = -500 - 0.5 * (nodes[s.x].z + nodes[s.y].z)  # constant total potential (hydraulic equilibrium)
+        rx = self.solve_dirichlet(sim_time, -15000, 0., p_s, cells = False)
+        jc = 0
+        for i in seg_ind:
+            jc -= self.axial_flux(i, sim_time, rx)
+        krs = jc / (-500 - 0.5 * (nodes[segs[0].x].z + nodes[segs[0].y].z) - rx[self.dirichlet_ind[0]])
+        return krs , jc
 
