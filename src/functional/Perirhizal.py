f49787c0799ac1792865dcf712564af230811e6e
Daniel Leitner <daniel.leitner@simwerk.at>
2023-03-16 09:40:10 +0100

    in progress

--- src/functional/Perirhizal.py
+++ src/functional/Perirhizal.py
@@ -7,6 +7,7 @@ from plantbox import MappedSegments
 import numpy as np
 from scipy.spatial import ConvexHull, Voronoi
 import scipy.linalg as la
+import vtk
 
 
 class PerirhizalPython(Perirhizal):
@@ -22,17 +23,18 @@ class PerirhizalPython(Perirhizal):
         super().__init__(ms)
 
     def get_default_volumes_(self):
-        """ returns the volumes of a rectangular grid (default) """
+        """ returns the soil volumes of a rectangular grid (default) """
         ms = self.ms  # rename
         width = ms.maxBound.minus(ms.minBound)
         vol = width.x * width.y * width.z / ms.resolution.x / ms.resolution.y / ms.resolution.z
         return np.ones((int(ms.resolution.x * ms.resolution.y * ms.resolution.z),)) * vol
 
     def get_density(self, type:str, volumes:list = []):
-        """ retrives length, surface or volume density
-            volumes of the finite volume cells, 
-            if list is empty a recangular grid is assumed (its data is stored in MappedSegments), 
-            see get_default_volumes_() """
+        """ retrives length, surface or volume density [cm/cm3, cm2/cm3, or cm3/cm3]
+            
+            type       length, surface or volume
+            volumes    soil volume cells, if empty a recangular grid is assumed (its data stored in MappedSegments), see get_default_volumes_() 
+        """
         if type == "length":
             f = lambda a, l: l  # cm
         elif type == "surface":
@@ -57,7 +59,11 @@ class PerirhizalPython(Perirhizal):
         return np.divide(d, volumes)  # cm/cm3, cm2/cm3, or cm3/cm3
 
     def get_outer_radii(self, type:str, volumes:list = []):
-        """ todo """
+        """ retrives the outer radii of the perirhizal zones [cm]
+            
+            type       each soil volume is splitted into perirhizal zones proportional to segment "length", "surface" or "volume"
+            volumes    soil volume cells, if empty a recangular grid is assumed (its data stored in MappedSegments), see get_default_volumes_() 
+        """
         if type == "length":
             return self.get_outer_radii_(type, volumes)  # Python
             # return super().segOuterRadii(2, volumes) # C++
@@ -72,7 +78,7 @@ class PerirhizalPython(Perirhizal):
             raise
 
     def get_outer_radii_(self, type:str, volumes:list = []):
-        """ python version of segOuterRadii """
+        """ python version of segOuterRadii, see get_outer_radii() """
         if type == "length":
             f = lambda a, l: l  # cm
         elif type == "surface":
@@ -91,7 +97,7 @@ class PerirhizalPython(Perirhizal):
         if not volumes:
             n = ms.resolution.x * ms.resolution.y * ms.resolution.z
             volumes = np.ones(int(n)) * (width.x * width.y * width.z) / n
-            # print("PerirhizalPython.get_outer_radii_() each volume has", (width.x * width.y * width.z) / n, "cm3")
+            print("PerirhizalPython.get_outer_radii_(): each soil volume has", (width.x * width.y * width.z) / n, "cm3")
 
         for cell_id, seg_ids in cell2seg.items():
             tt = np.sum(np.array([f(radii[i], lengths[i]) for i in seg_ids]))
@@ -103,57 +109,124 @@ class PerirhizalPython(Perirhizal):
         return outer_r
 
     def get_outer_radii_voronoi(self):
-        """ todo """
-
+        """ retrives the outer radii  [cm] and corresponding perirhizal volumes [cm3] 
+            based on a 3D Voronoi diagram
+        """
         min_b = self.ms.minBound
         max_b = self.ms.maxBound
         domain = pb.SDF_Cuboid(min_b, max_b)
-
         nodes_ = self.ms.nodes
-        nodes = np.array([[n.x, n.y, n.z] for n in nodes_])
-        segs = self.ms.segments
-        lengths = self.ms.segLength()
-
+        nodes = np.array([[n.x, n.y, n.z] for n in nodes_])  # to numpy array
         vor = Voronoi(nodes)
         vol = np.zeros(vor.npoints)
-        outer_r = np.zeros(vol.shape)
-        # quader, planes_quader = self.create_quader_from_bounding_box_([min_b.x, min_b.y, min_b.z], [max_b.x, max_b.y, max_b.z])
-        # print(quader, planes_quader)
 
-        for i, reg_num in enumerate(vor.point_region):
-            # print(i, reg_num)
+        for reg_num in vor.point_region:
             indices = vor.regions[reg_num]
-            if -1 in indices:  # some regions can be opened
-                vol[i] = -1
-            else:
-                inside = True
-                for j in indices:
-                    if domain.dist(vor.vertices[j]) >= 0:  # negative values mean inside the domain (sdf)
-                        inside = False
-                        break
-                if inside:
-                    vol[i] = ConvexHull(vor.vertices[indices]).volume
+            i = reg_num - 1
+            if i >= 0:
+                if -1 in indices:  # some regions can be opened
+                    vol[i] = -1
                 else:
-                    vol[i] = 0
+                    inside = True
+                    for j in indices:
+                        if domain.dist(vor.vertices[j]) >= 0:  # negative values mean inside the domain (sdf)
+                            inside = False
+                            break
+                    if inside:
+                        vol[i] = ConvexHull(vor.vertices[indices]).volume
+                    else:
+                        vol[i] = 0
 
-        print("shaping... ", vol.shape, nodes.shape, vor.npoints, len(vor.point_region), np.min(vor.point_region), np.max(vor.point_region))  # node indices
-        # one point too much in point_region?????
+        outer_r = vol.copy()
+        radii = self.ms.radii
+        lengths = self.ms.segLength()
+        for i, v in enumerate(vol[1:]):  # seg_index = node_index -1
+            if v > 0:
+                outer_r[i] = np.sqrt(v / (np.pi * lengths[i]) + radii[i] * radii[i])
 
-        dd
+        return outer_r
 
-        for si, seg in enumerate(segs):
-            v = vol[seg.y - 1]
-            outer_r[si] = np.sqrt(v / (np.pi * lengths[si]) + radii[si] * radii[si])
+    def get_voronoi_mesh(self, domain = None):
+        """
+        Creates a VTK grid containing all nodes, and only cells inside of domain 
+        """
+        min_b = self.ms.minBound
+        max_b = self.ms.maxBound
+        width = np.array([max_b.x, max_b.y, max_b.z]) - np.array([min_b.x, min_b.y, min_b.z])
+        if not domain:
+            domain = pb.SDF_Cuboid(min_b, max_b)
 
-        # for i, reg_num in enumerate(vor.point_region):
-        #     indices = vor.regions[reg_num]
-        #     if -1 in indices:  # some regions can be opened
-        #         vol[i] = 0
-        #     else:
-        #         vol[i] = ConvexHull(vor.vertices[indices]).volume
-        #         vol[i] = min(vol[i], 8.)
+        nodes_ = self.ms.nodes
+        nodes = np.array([[n.x, n.y, n.z] for n in nodes_])  # to numpy array
+        print(width)
+        print("*")
+        nodes = self.make_periodic_(nodes, width)
+        vor = Voronoi(nodes)
 
-        return outer_r  # TODO unfinished...
+        grid = vtk.vtkUnstructuredGrid()  # Create a VTK unstructured grid
+
+        points_array = vtk.vtkPoints()  # Add the Voronoi vertices as points
+        for v in vor.vertices:
+            points_array.InsertNextPoint(v)
+        grid.SetPoints(points_array)
+
+        vol = []
+        for reg_num in vor.point_region:
+            indices = vor.regions[reg_num]
+            i = reg_num - 1
+            if i >= 0:
+                if not -1 in indices:  # not an open region
+                    inside = True
+                    for j in indices:
+                        if domain.dist(vor.vertices[j]) >= 0:  # negative values mean inside the domain (sdf)
+                            inside = False
+                            break
+                    if inside:
+                        vol.append(ConvexHull(vor.vertices[indices]).volume)
+                        id_array = vtk.vtkIdList()
+                        for vertex_index in indices:
+                            id_array.InsertNextId(vertex_index)
+                        grid.InsertNextCell(vtk.VTK_CONVEX_POINT_SET, id_array)
+
+        cell_id = vtk.vtkDoubleArray()
+        cell_id.SetName("cell_id")
+        cell_id.SetNumberOfValues(len(vol))
+        for j in range(0, len(vol)):
+            cell_id.SetValue(j, j)
+        celldata = grid.GetCellData()
+        celldata.AddArray(cell_id)
+
+        vols = vtk.vtkDoubleArray()
+        vols.SetName("volumes")
+        vols.SetNumberOfValues(len(vol))
+        for j in range(0, len(vol)):
+            vols.SetValue(j, vol[j])
+        celldata = grid.GetCellData()
+        celldata.AddArray(vols)
+
+        return grid
+
+    def make_periodic_(self, nodes, width):
+        """
+        """
+        nodes_ = nodes.copy()
+        for n in nodes_:
+            n[0] = n[0] - ((n[0] + width[0] / 2.) // width[0]) * width[0]
+            n[1] = n[1] - ((n[1] + width[1] / 2.) // width[1]) * width[1]
+
+            if n[0] < -width[0] / 2:
+                print("n[0] < -width[0] / 2", n)
+
+            if n[0] > width[0] / 2:
+                print("n[0] > width[0] / 2", n)
+
+            if n[1] < -width[1] / 2:
+                print("n[1] < -width[0] / 2", n)
+
+            if n[1] > width[1] / 2:
+                print("n[1] > width[0] / 2", n)
+
+        return nodes_
 
     def intersect_polygon_quader_(self, polygon, quader):
         """ 
@@ -168,7 +241,7 @@ class PerirhizalPython(Perirhizal):
             p2 = polygon[(i + 1) % len(polygon)]
             normal = np.cross(p2 - p1, [0, 0, 1])
             d = -np.dot(normal, p1)
-            planes_polygon.append(np.hstack([normal, d]))
+            planes_polygon.append(np.hstaverck([normal, d]))
 
         # Check for intersection using separating axis theorem
         for plane in planes_polygon + planes_quader:
@@ -190,55 +263,4 @@ class PerirhizalPython(Perirhizal):
                     if 0 <= t <= 1:
                         intersection_points.append(p1 + t * (p2 - p1))
             return intersection_points
-
-    def get_voronoi_mesh(self):
-        grid = vtkUnstructuredGrid()  # Create a VTK unstructured grid
-
-        points_array = vtkPoints()  # Add the Voronoi vertices as points
-        for v in ver:
-            for j in range(0, 3):
-                v[j] = min(v[j], max_b[j])
-                v[j] = max(v[j], min_b[j])
-            points_array.InsertNextPoint(v)
-        grid.SetPoints(points_array)
-        print("voronoi nodes: ", ver.shape)
-
-        # print("vor.regions", vor.regions)
-        cc = 0
-        for region in vor.regions:
-            if len(region) > 0 and (-1 not in region):
-                cc += 1
-                id_array = vtk.vtkIdList()
-                id_array.InsertNextId(len(region))
-                # print("added an id array with length", len(region))
-                for vertex_index in region:
-                    id_array.InsertNextId(vertex_index)
-                grid.InsertNextCell(vtk.VTK_CONVEX_POINT_SET, id_array)
-
-        cell_id = vtk.vtkDoubleArray()
-        cell_id.SetName("cell_id")
-        cell_id.SetNumberOfValues(cc)
-        vc = 0
-        for j in range(0, cc):
-            cell_id.SetValue(vc, vc)
-            vc += 1
-        celldata = grid.GetCellData()
-        celldata.AddArray(cell_id)
-
-        cell_id = vtk.vtkDoubleArray()
-        cell_id.SetName("volumes")
-        cell_id.SetNumberOfValues(cc)
-        vc = 0
-        for j, region in enumerate(vor.regions):
-            if len(region) > 0 and (-1 not in region):
-                cell_id.SetValue(vc, vol0[j])
-                vc += 1
-        celldata = grid.GetCellData()
-        celldata.AddArray(cell_id)
-
-        # Write the VTK unstructured grid to a ParaView VTU file
-        writer = vtkXMLUnstructuredGridWriter()
-        writer.SetFileName('rootsystem.vtu')
-        writer.SetInputData(grid)
-        writer.Write()
 
