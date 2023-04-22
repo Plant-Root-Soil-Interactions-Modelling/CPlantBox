dc14af08420917f4370295fee2df8ceb7c991493
m-giraud <71639737+m-giraud@users.noreply.github.com>
2023-04-22 18:48:24 +0200

    Update installCPlantBox.py

--- installCPlantBox.py
+++ installCPlantBox.py
@@ -16,29 +16,7 @@ def show_message(message):
     print("*" * 120)
 
 
-def run_command(command):
-    with open("../installCPlantBox.log", "a") as log:
-        popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        for line in popen.stdout:
-            log.write(line)
-            print(line, end='')
-        for line in popen.stderr:
-            log.write(line)
-            print(line, end='')
-        popen.stdout.close()
-        popen.stderr.close()
-        return_code = popen.wait()
 
-
-def git_clone(url, branch=None):
-    clone = ["git", "clone"]
-    if branch:
-        clone += ["-b", branch]
-    result = run_command(command=[*clone, url])
-
-
-# clear the log file
-open('installCPlantBox.log', 'w').close()
 
 show_message("do not forget to run \n sudo apt update \n sudo apt upgrade \n\n only works for ubuntu >= 20.04")
 
@@ -47,8 +25,9 @@ show_message("do not forget to run \n sudo apt update \n sudo apt upgrade \n\n o
 ## (1/3) Check some prerequistes
 #################################################################
 #################################################################
+
 programs = ['wget', 'git', 'gcc', 'g++', 'cmake', 'pkg-config','clang', 'gfortran']#,'python3'] 
-show_message("(1/3) (a) Checking ubuntu prerequistes: " + " ".join(programs) + "...")
+show_message("(1/3) Checking ubuntu prerequistes: " + " ".join(programs) + "...")
 
 # check some prerequistes
 if (sys.version_info.major < 3) or (sys.version_info.minor < 7):
@@ -77,8 +56,8 @@ if len(error) > 0:
 import pip
 
 # check some prerequistes
-modules = ['numpy', 'scipy', 'matplotlib', 'vtk', 'mpi4py', 'astropy', 'pandas', 'pybind11'] 
-show_message("(1/3) (b) Checking python prerequistes: " + " ".join(modules) + "...")
+modules = ['numpy', 'scipy', 'matplotlib', 'vtk', 'mpi4py', 'astropy', 'pandas', 'pybind11[global]'] 
+show_message("(2/3) Checking python prerequistes: " + " ".join(modules) + "...")
 
 for mymodule in modules:
     #subprocess.run(["pip3", "install", mymodule]) 
@@ -90,12 +69,12 @@ for mymodule in modules:
         except:
             subprocess.run(["pip3", "install", mymodule]) 
       
-show_message("(1/3) Step completed. All prerequistes found.")
+show_message("(2/3) Step completed. All prerequistes found.")
 
 
 #################################################################
 #################################################################
-## (2/3) Clone modules
+## (3/3) Clone modules
 #################################################################
 #################################################################
 # make a new folder containing everything
@@ -104,17 +83,15 @@ os.chdir("CPB")
 
 # CPlantBox
 if not os.path.exists("CPlantBox"):
-    git_clone('https://github.com/Plant-Root-Soil-Interactions-Modelling/CPlantBox.git', branch = 'master')
-    os.chdir("CPlantBox")
-    subprocess.run(['cmake', '.']) 
-    subprocess.run(['make']) 
-    os.chdir("..")
+    subprocess.run(['git', 'clone', '-b', 'master', 'https://github.com/Plant-Root-Soil-Interactions-Modelling/CPlantBox.git'])
 else:
     print("-- Skip cloning CPlantBox because the folder already exists.")
-    os.chdir("CPlantBox")
-    subprocess.run(['cmake', '.']) 
-    subprocess.run(['make']) 
-    os.chdir("..")
+os.chdir("CPlantBox")
+if not os.path.exists("./src/external/pybind11"):
+    subprocess.run(['git', 'submodule', 'add', '-b', 'stable', '../../pybind/pybind11', 'src/external/pybind11'])
+subprocess.run(['cmake', '.']) 
+subprocess.run(['make']) 
+os.chdir("..")
 
 
 show_message("(3/3) Step completed. Succesfully configured and built CPlantBox.")
