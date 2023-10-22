9a06b3608ebf187271145eb39bb2d18d78c6f4e9
Daniel Leitner <d.leitner@fz-juelich.de>
2023-10-22 11:36:58 +0200

    cplantbox tutorial started

--- tutorial/examples/intro_basic.py
+++ tutorial/examples/intro_basic.py
@@ -4,8 +4,6 @@ import sys; sys.path.append("../.."); sys.path.append("../../src/")
 import plantbox as pb
 import visualisation.vtk_plot as vp
 
-import numpy as np
-
 # Create a new plant
 plant = pb.Plant()
 
@@ -18,7 +16,8 @@ plant.readParameters(path + name + ".xml")
 plant.initialize()
 
 # Simulate
-plant.simulate(40, verbose = True)
+simtime = 40  # days
+plant.simulate(simtime)
 
 # Export final result (as vtp)
 plant.write("results/example_plant.vtp")  # using polylines
@@ -27,4 +26,4 @@ ana = pb.SegmentAnalyser(plant)
 ana.write("results/example_plant_segs.vtp")  # using segments
 
 # Interactive plot, using vtk, press x, y, z to change view, r to reset view, g to save png
-vp.plot_plant(plant, "subType")
+vp.plot_plant(plant, "age")  # e.g. organType, subType, age
