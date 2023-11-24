37a2a4e1f98fc5f890106323a65f498f43621df4
Daniel Leitner <d.leitner@fz-juelich.de>
2023-11-24 13:46:27 +0100

    tutorial updates

--- tutorial/examples/topics_parameters.py
+++ tutorial/examples/topics_parameters.py
@@ -6,60 +6,24 @@ import visualisation.vtk_plot as vp
 import numpy as np
 
 plant = pb.Plant()
-
+root = pb.RootRandomParameter(plant)
 stem = pb.StemRandomParameter(plant)
-stem.subType = 1
-
 leaf = pb.LeafRandomParameter(plant)
-
 seed = pb.SeedRandomParameter(plant)
-# seed.seedPos = pb.Vector3d(0., 0., -3.)  # [cm] seed position
-# seed.maxB = 0  # [-] number of basal roots (neglecting basal roots and shoot borne)
-# seed.firstB = 10.  # [day] first emergence of a basal root
-# seed.delayB = 3.  # [day] delay between the emergence of basal roots
+
+print(stem)
+stem.subType = 1
+stem.lmax = 10
+stem.theta = 0.
+leaf.subType = 1
+root.subType = 1
+root.lmax = 100
 
 plant.setOrganRandomParameter(stem)
-# plant.setOrganRandomParameter(leaf)
+plant.setOrganRandomParameter(leaf)
 plant.setOrganRandomParameter(seed)
-
-p0 = pb.RootRandomParameter(plant)
-p1 = pb.RootRandomParameter(plant)
-p0.name = "taproot"
-p0.a = 0.2  # [cm] radius
-p0.subType = 1  # [-] index starts at 1
-p0.lb = 5  # [cm] basal zone
-p0.la = 10  # [cm] apical zone
-p0.lmax = 30  # [cm] maximal root length, number of lateral branching nodes = round((lmax-lb-la)/ln) + 1
-p0.ln = 1.  # [cm] inter-lateral distance (16 branching nodes)
-p0.theta = 0.  # [rad]
-p0.r = 1  # [cm/day] initial growth rate
-p0.dx = 10  # [cm] axial resolution
-# p0.successor = [[]]  # add successors
-# p0.successorP = [[1]]  # probability that successor emerges
-p0.tropismT = pb.TropismType.gravi  #
-p0.tropismN = 1.8  # [-] strength of tropism
-p0.tropismS = 0.2  # [rad/cm] maximal bending
-
-p1.name = "lateral"
-p1.a = 0.1  # [cm] radius
-p1.subType = 2  # [1] index starts at 1
-p1.lmax = 15  # # [cm] apical zone
-p1.lmaxs = 0.15  # [cm] standard deviation of the apical zone
-p1.theta = 90. / 180.*np.pi  # [rad]
-p1.r = 2  # initial growth rate
-p1.dx = 1  # [cm] axial resolution
-p1.tropismT = pb.TropismType.gravi  # exo
-p1.tropismN = 2  # [-] strength of tropism
-p1.tropismS = 0.1  # [rad/cm] maximal bending
-
-plant.setOrganRandomParameter(p0)
-print("successor", p0.successor)
-print("successorP", p0.successorP)
-plant.setOrganRandomParameter(p1)
+plant.setOrganRandomParameter(root)
 
 plant.initialize()
-
-plant.simulate(10)
-
-vp.plot_plant(plant, "subType")
-
+plant.simulate(20)
+vp.plot_plant(plant, "organType")
--- tutorial/examples/topics_tropism.py
+++ tutorial/examples/topics_tropism.py
@@ -5,35 +5,36 @@ import plantbox as pb
 import matplotlib.pyplot as plt
 import numpy as np
 
-N_ = [0, 0.5, 1, 3]  # strength [1]
-sigma_ = [0.1, 0.2, 0.4, 0.6]  # flexibility [1/m]
-dx = 1  # axial resolution
+N_ = [0, 0.5, 1, 2]  # strength [1]
+sigma_ = [0.1, 0.2, 0.4, 0.8]  # flexibility [1/m]
+dx = 0.5  # axial resolution
 theta = 70 / 180 * np.pi  # insertion angle [1]
+simtime = 65
 
-rs = pb.Plant()
-srp = pb.SeedRandomParameter(rs)
+plant = pb.Plant()
+srp = pb.SeedRandomParameter(plant)
 srp.firstB, srp.delayB, srp.maxB = 3, 3 , 100
-rs.setOrganRandomParameter(srp)
+plant.setOrganRandomParameter(srp)
 
-p0 = pb.RootRandomParameter(rs)
+p0 = pb.RootRandomParameter(plant)
 p0.name, p0.subType, p0.lmax, p0.r, p0.dx, p0.theta = "taproot", 1, 100, 1, dx, theta
 p0.tropismT = 1  # gravitropism
-rs.setOrganRandomParameter(p0)
+plant.setOrganRandomParameter(p0)
 
 fig, axes = plt.subplots(4, 4, figsize = (15, 10))
 for i, n in enumerate(N_):
     for j, sigma in enumerate(sigma_):
         a = axes[i][j]
-        rs.reset()  # does not delete parameters
+        plant.reset()  # does not delete parameters
 
         p0.tropismN = n
         p0.tropismS = sigma
 
-        rs.initializeLB()
-        rs.simulate(50, True)
+        plant.initializeLB(True)
+        plant.simulate(65, True)
 
-        nodes = rs.getNodes()
-        segs = rs.getSegments()
+        nodes = plant.getNodes()
+        segs = plant.getSegments()
         for s in segs:
             n1, n2 = nodes[s.x], nodes[s.y]
             a.plot([n1.x, n2.x], [n1.z, n2.z], "r")
