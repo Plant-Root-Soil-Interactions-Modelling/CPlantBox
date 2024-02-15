66afa7e428a24b6741f0c4b819ee3d50e3c19e69
m-giraud <m.giraud@fz-juelich.de>
2024-02-15 17:55:29 +0100

    pybind as module

--- installDumuxRosi_Ubuntu.py
+++ installDumuxRosi_Ubuntu.py
@@ -169,10 +169,7 @@ else:
 os.chdir("CPlantBox")
 
 
-if os.path.exists("./src/external/pybind11"):
-    subprocess.run(['rm', '-rf', 'src/external/pybind11'])#delete folder
-subprocess.run(['git', 'rm', '-r','--cached', 'src/external/pybind11'])#take out git cache for pybind11
-subprocess.run(['git', 'submodule', 'add',  '--force', '-b', 'stable', 'https://github.com/pybind/pybind11.git', './src/external/pybind11'])
+subprocess.run(['git', 'submodule', 'update',  '--recursive', '--init'])
 subprocess.run(['cmake', '.']) 
 subprocess.run(['make'])  
 os.chdir("..")
