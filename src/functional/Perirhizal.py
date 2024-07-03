7140ce28d044bbc8f6c520f453a02d3352b14cff
Daniel Leitner <d.leitner@fz-juelich.de>
2024-07-03 11:23:45 +0200

    example working

--- src/functional/Perirhizal.py
+++ src/functional/Perirhizal.py
@@ -3,33 +3,144 @@ import sys; sys.path.append(".."); sys.path.append("../..");
 import plantbox as pb
 from plantbox import Perirhizal
 from plantbox import MappedSegments
+import functional.van_genuchten as vg
 
 import numpy as np
+
 from scipy.spatial import ConvexHull, Voronoi
+from scipy.interpolate import RegularGridInterpolator
+from scipy.optimize import fsolve
 import scipy.linalg as la
-import vtk
 
 
 class PerirhizalPython(Perirhizal):
     """
-    Retrieve information on the perirhizal zones
+    Helper class for modelling the perirhizal zone 
     
-    Wraps MappedSegments (or specialisations MappedPlant, MappedRootsystem)
-    and adds functions to retrieve information on the perirhizal zones of single segments.
+    Wraps MappedSegments (or specialisations MappedPlant, MappedRootsystem) and adds functions to retrieve information 
+    
+    * calculates outer perirhizal radii    
+    * calculates root soil interface potential using steady rate approximation (Schröder et al. 2008)
+    * support of 4d lookup tables (file type is a zipped archive)
+    
+    run script to create a 4d lookup table (see __main__)
     """
 
-    def __init__(self, ms:MappedSegments):
-        """ @param ms is wrapped by this class """
-        super().__init__(ms)
+    def __init__(self, ms = None):
+        """  ms      reference to MappedSegments """
+        if ms:
+            super().__init__(ms)
+        else:
+            super().__init__()
 
-    def get_default_volumes_(self):
-        """ returns the soil volumes of a rectangular grid (default) """
-        ms = self.ms  # rename
-        width = ms.maxBound.minus(ms.minBound)
-        vol = width.x * width.y * width.z / ms.resolution.x / ms.resolution.y / ms.resolution.z
-        return np.ones((int(ms.resolution.x * ms.resolution.y * ms.resolution.z),)) * vol
+        self.lookup_table = None  # optional 4d look up table to find soil root interface potentials
+        self.sp = None  # corresponding van gencuchten soil parameter
 
-    def get_bounds(self, i:int, j:int, k:int):
+    def soil_root_interface_potentials(self, rx, sx, inner_kr, rho, sp):
+        """
+        finds matric potentials at the soil root interface for as all segments
+        uses a look up tables if present (see create_lookup, and open_lookup) 
+        
+        rx             xylem matric potential [cm]
+        sx             bulk soil matric potential [cm]
+        inner_kr       root radius times hydraulic conductivity [cm/day] 
+        rho            geometry factor [1] (outer_radius / inner_radius)
+        sp             soil van Genuchten parameters (type vg.Parameters), call 
+                       vg.create_mfp_lookup(sp) before 
+        """
+        assert len(rx) == len(sx) == len(inner_kr) == len(rho), "rx, sx, inner_kr, and rho must have the same length"
+        if self.lookup_table:
+            rsx = self.soil_root_interface_potentials_table(rx, sx, inner_kr, rho)
+        else:
+            rsx = np.array([PerirhizalPython.soil_root_interface_(rx[i], sx[i], inner_kr[i], rho[i], sp) for i in range(0, len(rx))])
+            rsx = rsx[:, 0]
+        return rsx
+
+    @staticmethod
+    def soil_root_interface_(rx, sx, inner_kr, rho, sp):
+        """
+        finds matric potential at the soil root interface for as single segment
+        
+        rx             xylem matric potential [cm]
+        sx             bulk soil matric potential [cm]
+        inner_kr       root radius times hydraulic conductivity [cm/day] 
+        rho            geometry factor [1] (outer_radius / inner_radius)
+        sp             soil van Genuchten parameters (type vg.Parameters), call 
+                       vg.create_mfp_lookup(sp) before 
+        """
+        k_soilfun = lambda hsoil, hint: (vg.fast_mfp[sp](hsoil) - vg.fast_mfp[sp](hint)) / (hsoil - hint)
+        # rho = outer_r / inner_r  # Eqn [5]
+        rho2 = rho * rho  # rho squaredswitched
+        # b = 2 * (rho2 - 1) / (1 + 2 * rho2 * (np.log(rho) - 0.5))  # Eqn [4]
+        b = 2 * (rho2 - 1) / (1 - 0.53 * 0.53 * rho2 + 2 * rho2 * (np.log(rho) + np.log(0.53)))  # Eqn [7]
+        fun = lambda x: (inner_kr * rx + b * sx * k_soilfun(sx, x)) / (b * k_soilfun(sx, x) + inner_kr) - x
+        rsx = fsolve(fun, (rx + sx) / 2)
+        return rsx
+
+    def create_lookup(self, filename, sp):
+        """      
+        Precomputes all soil root interface potentials for a specific soil type 
+        and saves results into a 4D lookup table
+        
+        filename       three files are written (filename, filename_, and filename_soil)
+        sp             van genuchten soil parameters, , call 
+                       vg.create_mfp_lookup(sp) before 
+        """
+        rxn = 150
+        rx_ = -np.logspace(np.log10(1.), np.log10(16000), rxn)
+        rx_ = rx_ + np.ones((rxn,))
+        rx_ = rx_[::-1]
+        sxn = 150
+        sx_ = -np.logspace(np.log10(1.), np.log10(16000), sxn)
+        sx_ = sx_ + np.ones((sxn,))
+        sx_ = sx_[::-1]
+        akrn = 100
+        akr_ = np.logspace(np.log10(1.e-7), np.log10(1.e-4), akrn)
+        rhon = 30
+        rho_ = np.logspace(np.log10(1.), np.log10(200.), rhon)
+        interface = np.zeros((rxn, sxn, akrn, rhon))
+        for i, rx in enumerate(rx_):
+            print(i)
+            for j, sx in enumerate(sx_):
+                for k, akr in enumerate(akr_):
+                    for l, rho in enumerate(rho_):
+                        interface[i, j, k, l] = PerirhizalPython.soil_root_interface_(rx, sx, akr, rho, sp)
+        np.savez(filename, interface = interface, rx_ = rx_, sx_ = sx_, akr_ = akr_, rho_ = rho_, soil = list(sp))
+        self.lookup_table = RegularGridInterpolator((rx_, sx_, akr_, rho_), interface)
+        self.sp = sp
+
+    def open_lookup(self, filename):
+        """ 
+        Opens a look-up table from a file to quickly find soil root interface potentials 
+        """
+        npzfile = np.load(filename + ".npz")
+        interface = npzfile["interface"]
+        rx_, sx_, akr_, rho_ = npzfile["rx_"], npzfile["sx_"], npzfile["akr_"], npzfile["rho_"]
+        soil = npzfile["soil"]
+        self.lookup_table = RegularGridInterpolator((rx_, sx_, akr_, rho_), interface)  # method = "nearest" fill_value = None , bounds_error=False
+        self.sp = vg.Parameters(soil)
+
+    def soil_root_interface_potentials_table(self, rx, sx, inner_kr_, rho_):
+        """
+        finds potential at the soil root interface using a lookup table
+            
+        rx             xylem matric potential [cm]
+        sx             bulk soil matric potential [cm]
+        inner_kr       root radius times hydraulic conductivity [cm/day] 
+        rho            geometry factor [1]
+        f              function to look up the potentials
+        """
+        try:
+            rsx = self.lookup_table((rx, sx, inner_kr_ , rho_))
+        except:
+            print("Look up failed: ")
+            print("rx", np.min(rx), np.max(rx))  # 0, -16000
+            print("sx", np.min(sx), np.max(loamsx))  # 0, -16000
+            print("inner_kr", np.min(inner_kr_), np.max(inner_kr_))  # 1.e-7 - 1.e-4
+            print("rho", np.min(rho_), np.max(rho_))  # 1. - 200.
+        return rsx
+
+    def get_cell_bounds(self, i:int, j:int, k:int):
         """ returns the bounding box (minx ,miny ,minz, maxx, maxy, maxz) of soil cell with index i, j, k of a rectangular grid """
         ms = self.ms  # rename
         width = ms.maxBound.minus(ms.minBound)
@@ -38,7 +149,7 @@ class PerirhizalPython(Perirhizal):
         max_ = min_ + cell_width
         return min_, max_
 
-    def nodes_within(self, nodes, min_, max_):
+    def nodes_within_box(self, nodes, min_, max_):
         """ returns the nodes inside the bounding box given by min_, and max_ """
         one = np.ones((nodes.shape[0], 1))
         n_, ni = [], []
@@ -49,20 +160,13 @@ class PerirhizalPython(Perirhizal):
         # print("nodes within", min_, max_, ":", len(n_))
         return np.array(n_), np.array(ni)
 
-    def flip_(self, nodes, center, axis):
-        """ flips the nodes around the center according axis """
-        n_ = nodes - np.ones((nodes.shape[0], 1)) @ center
-        n_[:, axis] = -n_[:, axis]
-        n_ = n_ + np.ones((nodes.shape[0], 1)) @ center
-        return n_
-
     def mirror(self, nodes, i:int, j:int, k:int):
         """ adds mirrored nodes to the 6 sides of the cube with index i, j, k"""
-        min_, max_ = self.get_bounds(i, j, k)
+        min_, max_ = self.get_cell_bounds(i, j, k)
         width_ = max_ - min_
         width_ = np.expand_dims(width_, axis = 0)
         center_ = min_ + width_ / 2.
-        n, ni = self.nodes_within(nodes, min_, max_)
+        n, ni = self.nodes_within_box(nodes, min_, max_)
         if n.shape[0] > 0:
             nodes_surr = n
             fipped_n = [self.flip_(n, center_, i_) for i_ in range(0, 3)]  # flip them ...
@@ -82,7 +186,7 @@ class PerirhizalPython(Perirhizal):
             return np.ones((0,)), np.ones((0,))
 
     def get_density(self, type:str, volumes:list = []):
-        """ retrives length, surface or volume density [cm/cm3, cm2/cm3, or cm3/cm3]
+        """ retrives length, surface or volume density [cm/cm3, cm2/cm3, or cm3/cm3] per soil cells
             
             type       length, surface or volume
             volumes    soil volume cells, if empty a recangular grid is assumed (its data stored in MappedSegments), see get_default_volumes_() 
@@ -138,7 +242,7 @@ class PerirhizalPython(Perirhizal):
         return d
 
     def get_outer_radii(self, type:str, volumes:list = []):
-        """ retrives the outer radii of the perirhizal zones [cm]
+        """ retrives the outer radii of the perirhizal zones [cm] 
             
             type       each soil volume is splitted into perirhizal zones proportional to segment "length", "surface" or "volume"
             volumes    soil volume cells, if empty a recangular grid is assumed (its data stored in MappedSegments), see get_default_volumes_() 
@@ -426,3 +530,36 @@ class PerirhizalPython(Perirhizal):
                     z.append(other[i])
         return y, z
 
+    def get_default_volumes_(self):
+        """ returns the soil volumes of a rectangular grid (default) """
+        ms = self.ms  # rename
+        width = ms.maxBound.minus(ms.minBound)
+        vol = width.x * width.y * width.z / ms.resolution.x / ms.resolution.y / ms.resolution.z
+        return np.ones((int(ms.resolution.x * ms.resolution.y * ms.resolution.z),)) * vol
+
+    def flip_(self, nodes, center, axis):
+        """ flips the nodes around the center according axis """
+        n_ = nodes - np.ones((nodes.shape[0], 1)) @ center
+        n_[:, axis] = -n_[:, axis]
+        n_ = n_ + np.ones((nodes.shape[0], 1)) @ center
+        return n_
+
+
+if __name__ == "__main__":
+
+    sand = [0.045, 0.43, 0.15, 3, 1000]
+    loam = [0.08, 0.43, 0.04, 1.6, 50]
+    clay = [0.1, 0.4, 0.01, 1.1, 10]
+    hydrus_loam = [0.078, 0.43, 0.036, 1.56, 24.96]
+    hydrus_clay = [0.068, 0.38, 0.008, 1.09, 4.8]
+    hydrus_sand = [0.045, 0.43, 0.145, 2.68, 712.8]
+    hydrus_sandyloam = [0.065, 0.41, 0.075, 1.89, 106.1]
+
+    # e.g. for loam
+    filename = "table_loam"
+    sp = vg.Parameters(loam)
+    vg.create_mfp_lookup(sp)
+    peri = PerirhizalPython()
+    peri.create_lookup(filename, sp)  # takes some hours
+
+    peri.open_lookup(filename)
--- tutorial/examples/drafts/example_sra_doussan.py
+++ tutorial/examples/drafts/example_sra_doussan.py
@@ -9,11 +9,17 @@ uses:
 
 Plant, MappedPlant (instead of RootSystem, MappedRootSystem) 
 PerirhizalPython class (for Schröder et al. 2008 related things)
+
+PlandHydraulicModel and PlantHydraulicParameters (instead of xylem_flux)
 """
+
 import plantbox as pb
 
-from functional.xylem_flux import XylemFluxPython  # Python hybrid solver
-from functional.Perirhizal import PerirhizalPython  # Steady rate helper
+# from functional.xylem_flux import XylemFluxPython  # Python hybrid solver
+from functional.PlantHydraulicParameters import PlantHydraulicParameters  # Doussan solver
+from functional.PlantHydraulicModel import PlantHydraulicModel  # Doussan solver
+
+from functional.perirhizal import PerirhizalPython  # Steady rate helper
 from functional.root_conductivities import *  # hard coded conductivities
 import functional.van_genuchten as vg
 from functional.xylem_flux import sinusoidal2
@@ -42,16 +48,22 @@ trans = 6.4  # cm3 /day (sinusoidal)
 wilting_point = -15000  # cm
 trans_f = lambda t, dt:-trans * sinusoidal2(t, dt)
 
-sim_time = 1  # [day] for task b
-rs_age = 10  # root system initial age
-age_dependent = False  # conductivities
-dt = 360. / (24 * 3600)  # [days] Time step must be very small
+sim_time = 7  # final simulation time [day]
+initial_age = 10  # initial age [day]
+age_dependent = True  # conductivities
+dt = 360. / (24 * 3600)  # [days]
 
+t = 0.  # current simulation time [day]
 N = round(sim_time / dt)
-t = 0.
 wilting_point = -15000  # cm
 skip = 1  # for output and results, skip iteration
 max_iter = 1000  # maximum for fix point iteration
+
+# SLOW
+soil_vg = vg.Parameters(loam)
+vg.create_mfp_lookup(soil_vg, wilting_point = -15000, n = 15001)
+peri = PerirhizalPython()
+peri.open_lookup("table_loam")
 
 """ Initialize macroscopic soil model """
 s = RichardsWrapper(RichardsSP())
@@ -65,7 +77,7 @@ s.initializeProblem()
 s.setCriticalPressure(wilting_point)
 
 """ Initialize xylem model """
-rs = pb.MappedPlant()  # pb.MappedPlant()
+rs = pb.MappedPlant()  # pb.MappedRootSystem()  #
 rs.readParameters(path + name + ".xml")
 if not periodic:
     sdf = pb.SDF_PlantBox(0.99 * (max_b[0] - min_b[0]), 0.99 * (max_b[1] - min_b[1]), max_b[2] - min_b[2])
@@ -73,16 +85,11 @@ else:
     sdf = pb.SDF_PlantBox(np.Inf, np.Inf, max_b[2] - min_b[2])
 rs.setGeometry(sdf)
 rs.initialize()
-rs.simulate(rs_age, True)
-r = XylemFluxPython(rs)
-init_conductivities(r, age_dependent)
+rs.simulate(initial_age, True)
 
-# soil look up
-soil_vg = None  # unused for look up table
-# soil_vg = vg.Parameters(loam) # comment in if no look up table is used
-# vg.create_mfp_lookup(soil_vg, wilting_point = -15000, n = 15001)
-peri = PerirhizalPython(rs)
-peri.open_lookup("table_loam")
+params = PlantHydraulicParameters()
+init_conductivities(params, age_dependent)
+r = PlantHydraulicModel("Doussan", rs, params)
 
 """ Coupling (map indices) """
 picker = lambda x, y, z: s.pick([x, y, z])
@@ -99,19 +106,23 @@ surf_ = [[], [], [], [], [], []]
 krs_ = []
 depth_ = []
 
+collar_ind = r.collar_index()  # segment index of root collar
+
 # initialize loop
-collar_ind = r.collar_index()
 mapping = rs.getSegmentMapper()  # because seg2cell is a dict
 sx = s.getSolutionHead_()  # richards.py
 hsb = np.array([sx[j] for j in mapping])  # soil bulk matric potential per segment
 rsx = hsb.copy()  # initial values for fix point iteration
-rx = r.solve(rs_age, trans_f(0, dt), 0., rsx, False, wilting_point, soil_k = [])
+r.update(initial_age)
+rx = r.solve(rsx, trans_f(0, dt), wilting_point)
 rx_old = rx.copy()
 
 N = int(np.ceil(sim_time / dt))  # number of iterations
 print("Starting simulation loop", N, "iterations")
 
-""" simulation loop """
+""" 
+    Simulation loop 
+"""
 for i in range(0, N):
 
     wall_iteration = timeit.default_timer()
@@ -120,9 +131,9 @@ for i in range(0, N):
 
     """ growth (and update everything)"""
 
-    rs.simulate(dt, verbose = False)
+    rs.simulate(dt, False)
 
-    outer_r = peri.get_outer_radii("length")  # 0: segment volume, 1: segment surface, 2: segment length
+    outer_r = PerirhizalPython(rs).seg_outer_radii("length")
     inner_r = rs.radii
     rho_ = np.divide(outer_r, np.array(inner_r))
     rho_ = np.minimum(rho_, np.ones(rho_.shape) * 200)  ############################################ (too keep within table)
@@ -131,18 +142,17 @@ for i in range(0, N):
 
     sx = s.getSolutionHead_()  # richards.py
     hsb = np.array([sx[j] for j in mapping])  # soil bulk matric potential per segment
-    rsx = hsb.copy()  # initial values for fix point iteration
-    hsb_ = hsb
+    hsb_ = hsb  # - cell_centers_z  # from total potential to matric potential
     hsb_ = np.maximum(hsb_, np.ones(hsb_.shape) * -15000.)  ############################################ (too keep within table)
     hsb_ = np.minimum(hsb_, np.zeros(hsb_.shape))  ############################################ (too keep within table)
 
-    rsx = np.hstack((rsx, hsb[rsx.shape[0]:]))
+    rsx = np.hstack((rsx, hsb[rsx.shape[0]:]))  # initial values for fix point iteration, last value and hsb for new segs
 
     # cell_centers = s.getCellCenters_()
     # cell_centers_z = np.array([cell_centers[j][2] for j in mapping])
     seg_centers_z = rs.getSegmentZ()
 
-    kr_ = r.getKr(rs_age + t)
+    kr_ = r.get_kr(initial_age + t)
     inner_kr_ = np.multiply(inner_r, kr_)  # multiply for table look up; here const
     inner_kr_ = np.maximum(inner_kr_, np.ones(inner_kr_.shape) * 1.e-7)  ############################################ (too keep within table)
     inner_kr_ = np.minimum(inner_kr_, np.ones(inner_kr_.shape) * 1.e-4)  ############################################ (too keep within table)
@@ -153,15 +163,16 @@ for i in range(0, N):
     err_ = 1.e6  # cm
     c = 0
 
-    # r.init_solve_static(rs_age + t, rsx, False, wilting_point, soil_k = [])  # LU factorisation for speed up
-    rx = r.solve(rs_age + t, trans_f(t, dt), 0., rsx, False, wilting_point, soil_k = [])
+    # r.init_solve_static(initial_age + t, rsx, False, wilting_point, soil_k = [])  # LU factorisation for speed up
+    r.update(initial_age + t)
+    rx = r.solve(rsx, trans_f(initial_age + t, dt), wilting_point)
     rx_old = rx.copy()
 
     while err_ > 1 and c < max_iter:
 
         """ interpolation """
         wall_interpolation = timeit.default_timer()
-        rx_ = rx[1:] - seg_centers_z  # from total potential to matric potential
+        rx_ = rx - seg_centers_z  # from total potential to matric potential
         rx_ = np.maximum(rx_, np.ones(rx_.shape) * -15000.)  ############################################ (too keep within table)
         rsx = peri.soil_root_interface_potentials(rx_ , hsb_, inner_kr_, rho_, soil_vg)
         rsx = rsx + seg_centers_z  # from matric potential to total potential
@@ -170,7 +181,8 @@ for i in range(0, N):
         """ xylem matric potential """
         wall_xylem = timeit.default_timer()
         # print("Segment size from Python ", len(r.rs.segments), ns)
-        rx = r.solve(rs_age + t, trans_f(rs_age + t, dt), 0., rsx, False, wilting_point, soil_k = [])  # xylem_flux.py, cells = False
+        rx = r.solve(rsx, trans_f(initial_age + t, dt), wilting_point)  # xylem_flux.py, cells = False
+        # print(r.last)
         err_ = np.linalg.norm(rx - rx_old)
         wall_xylem = timeit.default_timer() - wall_xylem
 
@@ -181,15 +193,16 @@ for i in range(0, N):
 
     """ macroscopic soil """
     wall_soil = timeit.default_timer()
-    fluxes = r.segFluxes(rs_age + t, rx, rsx, approx = False, cells = False)
-    collar_flux = r.collar_flux(rs_age + t, rx.copy(), rsx.copy(), k_soil = [], cells = False)  # validity checks
-    err = np.linalg.norm(np.sum(fluxes) - collar_flux)
-    if err > 1.e-6:
-        print("error: summed root surface fluxes and root collar flux differ" , err, r.neumann_ind, collar_flux, np.sum(fluxes))
-    err2 = np.linalg.norm(trans_f(rs_age + t, dt) - collar_flux)
-    if r.last == "neumann":
-        if err2 > 1.e-6:
-            print("error: potential transpiration differs root collar flux in Neumann case" , err2)
+    fluxes = r.radial_fluxes(rx, rsx)
+    # collar_flux = r.collar_flux(initial_age + t, rx.copy(), rsx.copy(), k_soil = [], cells = False)  # validity checks
+    # err = np.linalg.norm(np.sum(fluxes) - collar_flux)
+    # if err > 1.e-6:
+    #     print("error: summed root surface fluxes and root collar flux differ" , err, r.neumann_ind, collar_flux, np.sum(fluxes))
+    # err2 = np.linalg.norm(trans_f(initial_age + t, dt) - collar_flux)
+    # if r.last == "neumann":
+    #     if err2 > 1.e-6:
+    #         print("error: potential transpiration differs root collar flux in Neumann case" , err2)
+    print("fluxes", fluxes.shape)
     soil_fluxes = r.sumSegFluxes(fluxes)
     s.setSource(soil_fluxes.copy())  # richards.py
     s.solve(dt)
@@ -202,38 +215,38 @@ for i in range(0, N):
     sink = np.zeros(sx.shape)
     for k, v in soil_fluxes.items():
         sink[k] += v
-    t_.append(rs_age + t)  # day
+    t_.append(initial_age + t)  # day
     y_.append(np.sum(sink))  # cm3/day
 
     if i % skip == 0:
 
         # if i % (24 * skip) == 0:
-        print("time {:g}".format(rs_age + t), "{:g}/{:g} fix point iterations {:g}, {:g}".format(i, N, c, err_),
-              "wall times: fix point {:g}:{:g}; soil vs iter {:g}:{:g}".format(wall_interpolation / (wall_interpolation + wall_xylem), wall_xylem / (wall_interpolation + wall_xylem),
+        print("time {:g}".format(initial_age + t), "{:g}/{:g} fix point iterations {:g}, {:g}".format(i, N, c, err_),
+              "wall times: fzero vs. xylem {:g}:{:g}; fixpoint vs. soil {:g}:{:g}".format(wall_interpolation / (wall_interpolation + wall_xylem), wall_xylem / (wall_interpolation + wall_xylem),
                                                                                  wall_fixpoint / wall_iteration, wall_soil / wall_iteration),
-              "\nnumber of segments", rs.getNumberOfSegments(), "root collar", rx[0])
+              "\nnumber of segments", rs.getNumberOfSegments(), "root collar {:g} cm\n".format(rx[collar_ind]))
         # print("wall_interpolation", wall_interpolation)
 
-        sink_.append(sink)  # cm3/day (per soil cell)
-        psi_s2_.append(sx.copy())  # cm (per soil cell)
-        ana = pb.SegmentAnalyser(r.rs.mappedSegments())  # VOLUME and SURFACE
-        for j in range(0, 6):  # root types
-            anac = pb.SegmentAnalyser(ana)
-            anac.filter("subType", j)
-            vol_[j].append(anac.getSummed("volume"))
-            surf_[j].append(anac.getSummed("surface"))
-        krs, _ = r.get_krs(rs_age + t, [collar_ind])
-        krs_.append(krs)  # KRS
-        depth_.append(ana.getMinBounds().z)
+        # sink_.append(sink)  # cm3/day (per soil cell)
+        # psi_s2_.append(sx.copy())  # cm (per soil cell)
+        # ana = pb.SegmentAnalyser(r.rs.mappedSegments())  # VOLUME and SURFACE
+        # for j in range(0, 6):  # root types
+        #     anac = pb.SegmentAnalyser(ana)
+        #     anac.filter("subType", j)
+        #     vol_[j].append(anac.getSummed("volume"))
+        #     surf_[j].append(anac.getSummed("surface"))
+        # krs, _ = r.get_krs(initial_age + t, [collar_ind])
+        # krs_.append(krs)  # KRS
+        # depth_.append(ana.getMinBounds().z)
 
         """ direct vtp output """
         # psi_x_.append(rx.copy())  # cm (per root node)
         # psi_s_.append(rsx.copy())  # cm (per root segment)
         # ana.addData("rx", rx[1:])
         # ana.addData("rsx", rsx)
-        # ana.addAge(rs_age + t)  # "age"
-        # ana.addConductivities(r, rs_age + t)  # "kr", "kx"
-        # ana.addFluxes(r, rx, rsx, rs_age + t)  # "axial_flux", "radial_flux"
+        # ana.addAge(initial_age + t)  # "age"
+        # ana.addConductivities(r, initial_age + t)  # "kr", "kx"
+        # ana.addFluxes(r, rx, rsx, initial_age + t)  # "axial_flux", "radial_flux"
         # ana.write("results/rs{0:05d}.vtp".format(int(i / skip)), ["radius", "subType", "creationTime", "organType", "rx", "rsx", "age", "kr", "kx", "axial_flux", "radial_flux"])
 
 print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
@@ -255,5 +268,6 @@ plt.show()
 np.savez("example_sra", time = t_, actual = y_, cumulative = np.cumsum(-np.array(y_) * dt), krs = krs_)
 
 """ VTK visualisation """
+print(rx.shape)
 vp.plot_roots_and_soil(r.rs, "pressure head", rx, s, periodic, np.array(min_b), np.array(max_b), cell_number, name)
 
