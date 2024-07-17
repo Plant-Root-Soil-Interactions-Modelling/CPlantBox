94c3b61c86d65c346cea106d3d3bc2e4a6346786
Dirk Helmrich <dnh2@hi.is>
2024-07-17 10:15:26 +0200

    Updates example vis for testing

--- tutorial/examples/example_vis.py
+++ tutorial/examples/example_vis.py
@@ -1,6 +1,11 @@
 """root system length over time"""
 import sys; sys.path.append("../.."); sys.path.append("../../src/"); sys.path.append("./"); sys.path.append("./src/")
 
+import os
+import sys
+# call cmake --build on ../../
+os.system("cmake --build ../../ --config Release -j 4")
+
 import plantbox as pb
 import visualisation.vtk_plot as vp
 import visualisation.vis_tools as cpbvis
@@ -10,51 +15,50 @@ import numpy as np
 filename = "../../modelparameter/structural/plant/fspm2023.xml"
 output = "./results/vis_plant"
 
-time = 28
-leaf_res = 30
 # create a plant
 plant = pb.MappedPlant()
 plant.readParameters(filename)
-# vis = pb.PlantVisualiser(plant)
+vis = pb.PlantVisualiser(plant)
 
-# for p in plant.getOrganRandomParameter(pb.stem):
-#   p.r = 0.758517633
-#   p.lb = 4
-#   # p.delayLat = 4
-#   p.lmax = (time - 7) * p.r
-#   p.dx = 0.1
-# for p in plant.getOrganRandomParameter(pb.leaf):
-#   p.la, p.lmax = 38.41053981, 38.41053981
-#   # p.theta = 0.2 # 0.2
-#   p.theta = 0.01
-#   p.tropismT = 1  # 6: Anti-gravitropism to gravitropism
-#   p.areaMax = 54.45388021  # cm2, area reached when length = lmax
-#   phi = np.array([-90, -80, -45, 0., 45, 90]) / 180. * np.pi
-#   l = np.array([38.41053981, 1 , 1, 0.3, 1, 38.41053981])  # distance from leaf center
-#   p.leafGeometryPhi = phi
-#   p.leafGeometryX = l
-#   # p.tropismN = 5
-#   p.tropismS = 0.08
-#   p.tropismAge = 5  # < age at which tropism switch occures, only used if p.tropismT = 6
-#   p.createLeafRadialGeometry(phi, l, leaf_res)
 
 # Initialize
 plant.initialize()
-# vis.SetGeometryResolution(8)
-# vis.SetLeafResolution(leaf_res)
+vis.SetGeometryResolution(8)
+vis.SetLeafResolution(30)
+vis.SetComputeMidlineInLeaf(False)
 
 # Simulate
-plant.simulate(time, True)
+plant.simulate(28, False)
 
-# vis.ResetGeometry()
-# vis.ComputeGeometryForOrganType(pb.stem, False)
-# vis.ComputeGeometryForOrganType(pb.leaf, False)
+for o in plant.getOrgans() :
+  if o.organType() == pb.leaf :
+    print("Plant Organ: ", o.getId())
+
+vis.ResetGeometry()
+vis.ComputeGeometryForOrgan(2871)
 
 # Write the geometry to file#
-# data = cpbvis.PolydataFromPlantGeometry(vis)
-# cpbvis.WritePolydataToFile(data, output + ".vtp")
+data = cpbvis.PolydataFromPlantGeometry(vis)
 
-# vp.plot_plant(plant, "subType")
+# view the vtk geometry
+import vtk
+renderer = vtk.vtkRenderer()
+window = vtk.vtkRenderWindow()
+window.AddRenderer(renderer)
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(window)
 
-vp.write_plant("test", plant)  # will write test.vtp (with centerlines), and test_leafs.vtp (as polygones)
-print("fin")
+# Add the data to the renderer
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputData(data)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(0.0, 1.0, 1.0)
+renderer.AddActor(actor)
+
+# Render
+interactor.Initialize()
+window.Render()
+interactor.Start()
+
+
