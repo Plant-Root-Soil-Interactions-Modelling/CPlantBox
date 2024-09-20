98710b0e02307d05a241746d734049f55fdd88e3
Daniel Leitner <d.leitner@fz-juelich.de>
2024-09-20 10:26:53 +0200

    added examples

--- tutorial/examples/drafts/example_sra.py
+++ tutorial/examples/drafts/example_sra.py
@@ -14,6 +14,7 @@ for a growing root system
 import plantbox as pb
 
 from functional.PlantHydraulicParameters import PlantHydraulicParameters
+from functional.PlantHydraulicModel import PlantHydraulicModel
 from functional.PlantHydraulicModel import HydraulicModel_Doussan
 from functional.PlantHydraulicModel import HydraulicModel_Meunier
 from functional.Perirhizal import PerirhizalPython  # Steady rate helper
@@ -24,6 +25,7 @@ import visualisation.vtk_plot as vp
 
 from rosi_richards import RichardsSP  # RichardsSP  # C++ part (Dumux binding)
 from richards import RichardsWrapper  # Python part
+from rhizo_models import plot_transpiration
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,27 +34,27 @@ import timeit
 """ Parameters """
 min_b = [-4., -4., -25.]
 max_b = [4., 4., 0.]
-cell_number = [1, 1, 25]  # [16, 16, 30]  # [32, 32, 60]
+cell_number = [8, 8, 25]  # [16, 16, 30]  # [32, 32, 60]
 periodic = True
 
 path = "../../../../CPlantBox/modelparameter/structural/rootsystem/"
-name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010
+name = "Anagallis_femina_Leitner_2010"  # "Zea_mays_1_Leitner_2010"  # "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010
 loam = [0.08, 0.43, 0.04, 1.6, 50]  # make sure these vg parameters correspond to the look up table
 initial = -659.8  # total potential [cm]
 
 trans = 6.4  # [cm3/day] sinusoidal
 wilting_point = -15000  # cm
-trans_f = lambda t, dt:-trans * HydraulicModel_Doussan.sinusoidal2(t, dt)  # rename
+trans_f = lambda t, dt:-trans * PlantHydraulicModel.sinusoidal2(t, dt)  # rename
 
 sim_time = 1  # simulation time [day]
-initial_age = 10  # initial age [day]
+initial_age = 10  # 10  # initial age [day]
 age_dependent = False  # conductivities
 dt = 360. / (24 * 3600)  # [days] Time step must be very small
 
 N = round(sim_time / dt)  # number of iterations
 t = 0.  # current simulation time [day]
 skip = 1  # for output and results, skip iteration
-max_iter = 1000  # maximum for fix point iteration
+max_iter = 100  # maximum for fix point iteration
 
 """ Initialize macroscopic soil model """
 s = RichardsWrapper(RichardsSP())
@@ -62,6 +64,9 @@ s.setHomogeneousIC(initial, True)  # cm pressure head, equilibrium
 s.setTopBC("noFlux")
 s.setBotBC("noFlux")
 s.setVGParameters([loam])
+s.setParameter("Newton.EnableChop", "True")
+s.setParameter("Newton.EnableAbsoluteResidualCriterion", "True")
+s.setParameter("Soil.SourceSlope", "1000")  # turns regularisation of the source term on
 s.initializeProblem()
 s.setCriticalPressure(wilting_point)
 
@@ -79,30 +84,31 @@ rs.simulate(initial_age, True)
 """ Initialize plant hydraulic and perirhizal model"""
 params = PlantHydraulicParameters()
 init_conductivities(params, age_dependent)
+# params.plot_conductivities()
 
-r = HydraulicModel_Doussan(rs, params, cached = True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
-rs.setRectangularGrid(pb.Vector3d(min_b), pb.Vector3d(max_b), pb.Vector3d(cell_number), False)
+# fname = "../../../../dumux-rosi/grids/RootSystem8.rsml"
+r = HydraulicModel_Meunier(rs, params, cached = True)  # or HydraulicModel_Doussan, HydraulicModel_Meunier
+r.ms.setRectangularGrid(pb.Vector3d(min_b), pb.Vector3d(max_b), pb.Vector3d(cell_number), False)
 picker = lambda x, y, z: s.pick([x, y, z])
 r.ms.setSoilGrid(picker)  # maps segment
 
-peri = PerirhizalPython(rs)
+r.test()
+vp.plot_roots(rs, "subType")
+
+peri = PerirhizalPython(r.ms)
 peri.open_lookup("../table_loam")
 
 """ Numerical solution """
 start_time = timeit.default_timer()
 
-psi_x_, psi_s_, sink_ , t_, y_, psi_s2_ = [], [], [], [], [], []  # for post processing
+psi_x_, psi_s_, sink_ , t_, y_, z_, psi_s2_ = [], [], [], [], [], [], []  # for post processing
 vol_ = [[], [], [], [], [], []]
 surf_ = [[], [], [], [], [], []]
 krs_ = []
 depth_ = []
 
-# initialize loop
-collar_ind = r.collar_index()  # segment index of root collar (collar node index == 0)
-
 psi_s_cell = s.getSolutionHead_()  # richards.py
 psi_s = np.array(r.ms.getHs(psi_s_cell))
-
 psi_rs = psi_s.copy()  # initial values for fix point iteration
 
 N = int(np.ceil(sim_time / dt))  # number of iterations
@@ -116,24 +122,21 @@ for i in range(0, N):
     t = i * dt  # current simulation time
 
     """ growth (and update everything)"""
+    # rs.simulate(dt, verbose = False)
 
-    rs.simulate(dt, verbose = False)
+    psi_s_cell = s.getSolutionHead_()  # richards.py
+    psi_s = np.array(r.ms.getHs(psi_s_cell))
+    psi_s = np.maximum(psi_s, np.ones(psi_s.shape) * -15000.)  ############################################ (too keep within table)
+    psi_s = np.minimum(psi_s, np.zeros(psi_s.shape))  ############################################ (too keep within table)
 
     outer_r = peri.get_outer_radii("length")  # 0: segment volume, 1: segment surface, 2: segment length
-    inner_r = rs.radii
+    inner_r = r.ms.radii
     rho_ = np.divide(outer_r, np.array(inner_r))
     rho_ = np.minimum(rho_, np.ones(rho_.shape) * 200)  ############################################ (too keep within table)
     kr_ = r.get_kr(initial_age + t)
     inner_kr_ = np.multiply(inner_r, kr_)  # multiply for table look up; here const
     inner_kr_ = np.maximum(inner_kr_, np.ones(inner_kr_.shape) * 1.e-7)  ############################################ (too keep within table)
     inner_kr_ = np.minimum(inner_kr_, np.ones(inner_kr_.shape) * 1.e-4)  ############################################ (too keep within table)
-
-    mapping = rs.getSegmentMapper()
-
-    psi_s_cell = s.getSolutionHead_()  # richards.py
-    psi_s = np.array([psi_s_cell[j] for j in mapping])  # soil bulk matric potential per segment
-    psi_s = np.maximum(psi_s, np.ones(psi_s.shape) * -15000.)  ############################################ (too keep within table)
-    psi_s = np.minimum(psi_s, np.zeros(psi_s.shape))  ############################################ (too keep within table)
 
     psi_rs = np.hstack((psi_rs, psi_s[psi_rs.shape[0]:]))  # initial values for fix point iteration
 
@@ -160,10 +163,11 @@ for i in range(0, N):
 
         """ xylem matric potential """
         wall_xylem = timeit.default_timer()
-        psi_x = r.solve(initial_age + t, trans_f(initial_age + t, dt), psi_rs, cells = False)  # xylem_flux.py, cells = False
-        err_ = np.linalg.norm(psi_x - psi_x_old)
+        psi_x = r.solve_again(initial_age + t, trans_f(initial_age + t, dt), psi_rs, cells = False)  # xylem_flux.py, cells = False
         wall_xylem = timeit.default_timer() - wall_xylem
 
+        err_ = np.linalg.norm(psi_x - psi_x_old)
+        psi_x_old = psi_x.copy()
         c += 1
 
     wall_fixpoint = timeit.default_timer() - wall_fixpoint
@@ -175,13 +179,20 @@ for i in range(0, N):
     err = np.linalg.norm(np.sum(fluxes) - collar_flux)
     if err > 1.e-6:
         print("error: summed root surface fluxes and root collar flux differ" , err, r.neumann_ind, collar_flux, np.sum(fluxes))
-    err2 = np.linalg.norm(trans_f(initial_age + t, dt) - collar_flux)
-    if r.last == "neumann":
+    if psi_x[0] > -14999:
+        err2 = np.linalg.norm(trans_f(initial_age + t, dt) - collar_flux)
         if err2 > 1.e-6:
             print("error: potential transpiration differs root collar flux in Neumann case" , err2)
+            print(trans_f(initial_age + t, dt))
+            print(collar_flux)
+            raise
+
     soil_fluxes = r.sumSegFluxes(fluxes)
+
+    water = s.getWaterVolume()
     s.setSource(soil_fluxes.copy())  # richards.py
     s.solve(dt)
+    soil_water = (s.getWaterVolume() - water) / dt
 
     wall_soil = timeit.default_timer() - wall_soil
 
@@ -192,7 +203,8 @@ for i in range(0, N):
     for k, v in soil_fluxes.items():
         sink[k] += v
     t_.append(initial_age + t)  # day
-    y_.append(np.sum(sink))  # cm3/day
+    y_.append(collar_flux)  # cm3/day
+    z_.append(soil_water)
 
     if i % skip == 0:
 
@@ -200,7 +212,7 @@ for i in range(0, N):
         print("time {:g}".format(initial_age + t), "{:g}/{:g} fix point iterations {:g}, {:g}".format(i, N, c, err_),
               "wall times: fix point {:g}:{:g}; soil vs iter {:g}:{:g}".format(wall_interpolation / (wall_interpolation + wall_xylem), wall_xylem / (wall_interpolation + wall_xylem),
                                                                                  wall_fixpoint / wall_iteration, wall_soil / wall_iteration),
-              "\nnumber of segments", rs.getNumberOfSegments(), "root collar", psi_x[collar_ind])
+              "\nnumber of segments", r.ms.getNumberOfMappedSegments(), "root collar", psi_x[0], "transpiration", trans_f(initial_age + t, dt))
         # print("wall_interpolation", wall_interpolation)
 
         # sink_.append(sink)  # cm3/day (per soil cell)
@@ -227,24 +239,12 @@ for i in range(0, N):
 
 print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
 
-""" save final results """
-
 """ transpiration over time """
-fig, ax1 = plt.subplots()
-ax1.plot(t_, [trans * sinusoidal2(t, dt) for t in t_], 'k')  # potential
-ax1.plot(t_, -np.array(y_), 'g')  # actual
-ax2 = ax1.twinx()
-cum_y = np.cumsum(-np.array(y_) * dt)
-ax2.plot(t_, cum_y, 'c--')  # cumulative
-ax1.set_xlabel("Time [d]")
-ax1.set_ylabel("Transpiration $[cm^3 d^{-1}]$")
-ax1.legend(['Potential', 'Actual', 'Cumulative'], loc = 'upper left')
-np.savetxt(name, np.vstack((t_, -np.array(y_))), delimiter = ';')
-print("total uptake of", cum_y[-1], "[cm3]")
-plt.show()
+plot_transpiration(t_, y_, z_, lambda t: trans * PlantHydraulicModel.sinusoidal2(t, dt))
 
+""" save final results """
 np.savez("example_sra", time = t_, actual = y_, cumulative = np.cumsum(-np.array(y_) * dt), krs = krs_)
 
 """ VTK visualisation """
-vp.plot_roots_and_soil(r.rs, "pressure head", psi_x, s, periodic, np.array(min_b), np.array(max_b), cell_number, name)
+vp.plot_roots_and_soil(r.ms, "pressure head", psi_x, s, periodic, np.array(min_b), np.array(max_b), cell_number, name)
 
