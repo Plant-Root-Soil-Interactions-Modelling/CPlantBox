be992aeb64c71e377b18b4c07b7faa6a3d8f0a4e
annasheck <anna.heck@outlook.com>
2024-10-17 10:52:08 +0200

    transfer file updated

--- modelparameter/rootsys2plant.py
+++ modelparameter/rootsys2plant.py
@@ -6,30 +6,32 @@ import plantbox as pb
 
 plant_path = "structural/plant"
 root_path = "structural/rootsystem"
-filename = "Bench_lupin"
-
-plant = pb.Plant()
-plant.readParameters(root_path +"/"+ filename +".xml",verbose = True)
-plant.writeParameters("testOfWrite.xml")
 
 
 plant_directory = os.fsencode(plant_path)
-# root_directory = os.fsencode(root_path)
+root_directory = os.fsencode(root_path)
 
-# for plant_file in os.listdir(plant_directory):
-#     filename = os.fsdecode(plant_file)
-#     if filename.endswith(".xml") and not os.path.isfile(filename):
-#          print(filename)
-#          p = pb.Plant()
-#          p.readParameters(os.path.join(plant_path, filename))
-#          p.writeParameters(filename)
+fileswerrors = []
+
+for plant_file in os.listdir(plant_directory):
+    filename = os.fsdecode(plant_file)
+    if filename.endswith(".xml"):
+        try:
+            p = pb.Plant()
+            p.readParameters(os.path.join(plant_path, filename))
+            p.writeParameters(filename)
+        except:
+            fileswerrors.append(filename)
 
 
-# for root_file in os.listdir(root_directory):
-#     filename = os.fsdecode(root_file)
-#     if filename.endswith(".xml") and not os.path.isfile(filename):
-#          print(filename)
-#          p = pb.Plant()
-#          p.readParameters(os.path.join(root_path, filename))
-#          p.writeParameters(filename)
 
+for root_file in os.listdir(root_directory):
+    filename = os.fsdecode(root_file)
+    if filename.endswith(".xml") and not os.path.isfile(filename):
+         print(filename)
+         p = pb.Plant()
+         p.readParameters(os.path.join(root_path, filename))
+         p.writeParameters(filename)
+
+
+print(*fileswerrors, sep="\n")
