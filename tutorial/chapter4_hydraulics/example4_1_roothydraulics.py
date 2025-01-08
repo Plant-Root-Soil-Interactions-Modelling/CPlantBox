bad0ef2cb7c2ded19d603f10222bbfe282476a7a
Daniel Leitner <d.leitner@fz-juelich.de>
2025-01-08 16:13:49 +0100

    labels added

--- tutorial/chapter4_hydraulics/example4_1_roothydraulics.py
+++ tutorial/chapter4_hydraulics/example4_1_roothydraulics.py
@@ -10,48 +10,48 @@ from functional.PlantHydraulicModel import HydraulicModel_Meunier
 import numpy as np
 import matplotlib.pyplot as plt
 
-""" Parameters """
+""" Parameters """  # |\label{e41:parameters}
 initial_age = 14  # root system age [day]
 kx = 4.32e-2  # axial conductivity [cm3/day]
 kr = 1.728e-4  # radial conductivity [1/day]
 Hs = -300  # soil total potential [cm]
-p0 = -1000  # dirichlet bc at top [cm]
-t_pot = -1  # potential plant transpiration [cm3/day]
+h0 = -1000  # dirichlet bc at top [cm] |\label{e41:h0}
+t_pot = -1  # potential plant transpiration [cm3/day] |\label{e41:t_pot}
 
-""" root system """
-plant = pb.MappedPlant()
+""" root system """  # |\label{e41:rootsystem}
+plant = pb.MappedPlant()  # |\label{e41:mappedplant}
 path = "../../modelparameter/structural/rootsystem/"
-name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010
+name = "Anagallis_femina_Leitner_2010"
 plant.readParameters(path + name + ".xml")
 plant.initialize()
-plant.simulate(initial_age, False)
+plant.simulate(initial_age, False)  # |\label{e41:rootsystem_end}
 
 """ root hydraulic properties """
-params = PlantHydraulicParameters()
+params = PlantHydraulicParameters()  # |\label{e41:hydraulicparams}
 params.set_radial_conductivity(kr)
 params.set_axial_conductivity(kx)
-r = HydraulicModel_Doussan(plant, params)  # hydraulic model
+hm = HydraulicModel_Doussan(plant, params)  # |\label{e41:model}
+hm = HydraulicModel_Meunier(plant, params)
 
-""" Numerical solution """
+""" Numerical solution """  # |\label{e41:numerical}
 ns = plant.getNumberOfMappedSegments()
-hsr = plant.total2matric(Hs * np.ones((ns,)))
-
-hx = r.solve_dirichlet(initial_age, p0, hsr, cells = False)
-print("Root collar potential {:g} [cm], transpiration {:g} [cm3/day]".format(hx[0], r.get_transpiration(initial_age, hx, hsr)))
-# hx = r.solve_neumann(initial_age, t_pot, hsr, cells = False)
-# print("Root collar potential {:g} [cm], transpiration {:g} [cm3/day]".format(hx[0], r.get_transpiration(initial_age, hx, hsr)))
-
-fluxes = r.radial_fluxes(initial_age, hx, hsr, False)  # cm3/day
+hsr = plant.total2matric(Hs * np.ones((ns,)))  # |\label{e41:hsr}
+hx = hm.solve_dirichlet(initial_age, h0, hsr, cells = False)  # |\label{e41:dirichlet}
+print("Root collar potential {:g} [cm], transpiration {:g} [cm3/day]".format(hx[0], hm.get_transpiration(initial_age, hx, hsr)))
+hx = hm.solve_neumann(initial_age, t_pot, hsr, cells = False)  # |\label{e41:neumann}
+print("Root collar potential {:g} [cm], transpiration {:g} [cm3/day]".format(hx[0], hm.get_transpiration(initial_age, hx, hsr)))
 
 """ Additional vtk plot """
-ana = pb.SegmentAnalyser(r.ms.mappedSegments())
+ana = pb.SegmentAnalyser(hm.ms.mappedSegments())  # |\label{e41:sa}
 ana.addData("hx", hx)  # xylem potentials [cm]
-ana.addData("SUF", r.get_suf(initial_age))  # standard uptake fraction [1]
-ana.addAge(initial_age)  # age [day]
-ana.addHydraulicConductivities(params, initial_age)  # kr [1/day], kx [cm3/day]
-ana.addFluxes(r, hx, hsr, initial_age)  # "axial_flux" [cm3/day], "radial_flux" [ (cm3/cm2) / day]
+ana.addData("SUF", hm.get_suf(initial_age))  # standard uptake fraction [1]
+ana.addAge(initial_age)  # age [day] |\label{e41:age}
+ana.addHydraulicConductivities(params, initial_age)  # kr [1/day], kx [cm3/day] |\label{e41:conductivities}
+ana.addFluxes(hm, hx, hsr, initial_age)  # "axial_flux" [cm3/day], "radial_flux" [ (cm3/cm2) / day] |\label{e41:fluxes}
 vp.plot_roots(ana, "radial_flux")
 
 """ output for paraview """
-ana.write("example4_1_root_hydraulics.vtp",
+ana.write("example4_1_roothydraulics.vtp",  # |\label{e41:paraview}
           types = ["radius", "subType", "age", "hx", "SUF", "kr", "kx", "axial_flux", "radial_flux"])
+
+# q_radial = hm.radial_fluxes(initial_age, hx, hsr, False)  # [cm3/day] |\label{e41:neumann}
