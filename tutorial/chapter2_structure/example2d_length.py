1c6ff75a582d82c3a5ebaeec29aa85e0cf6f5b39
m-giraud <m.giraud@fz-juelich.de>
2025-01-27 11:49:46 +0100

    update tutorial

--- tutorial/chapter2_structure/example2d_length.py
+++ tutorial/chapter2_structure/example2d_length.py
@@ -1,12 +1,12 @@
 """root system length over time"""
-import sys; sys.path.append("../.."); sys.path.append("../../src/")
+import sys; sys.path.append("../.."); sys.path.append("../../src/") # |\label{l2_2d:importStart}|
 
 import plantbox as pb
 
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # |\label{l2_2d:importEnd}|
 
-path = "../../modelparameter/structural/rootsystem/"
+path = "../../modelparameter/structural/rootsystem/"  # |\label{l2_2d:defineStart}|
 name = "Brassica_napus_a_Leitner_2010"
 
 rs = pb.Plant()
@@ -15,15 +15,15 @@ rs.initialize()
 
 simtime = 60.  # days
 dt = 1.
-N = round(simtime / dt)  # steps
+N = round(simtime / dt)  # steps # |\label{l2_2d:defineEnd}|
 
 # Plot some scalar value over time
-stype = "length"
+stype = "length" # |\label{l2_2d:plotStart}|
 v_, v1_, v2_, v3_ = np.zeros(N), np.zeros(N), np.zeros(N), np.zeros(N)
 for i in range(0, N):
     rs.simulate(dt)
-    t = np.array(rs.getParameter("type"))
-    v = np.array(rs.getParameter(stype))
+    t = np.array(rs.getParameter("subType")) # |\label{l2_2d:getParam1}|
+    v = np.array(rs.getParameter(stype))  # |\label{l2_2d:getParam2}|
     v_[i] = np.sum(v)
     v1_[i] = np.sum(v[t == 1])
     v2_[i] = np.sum(v[t == 2])
@@ -35,4 +35,4 @@ plt.xlabel("time (days)")
 plt.ylabel(stype + " (cm)")
 plt.legend(["total", "tap root", "lateral", "2. order lateral"])
 plt.savefig("results/example_2d.png")
-plt.show()
+plt.show() # |\label{l2_2d:plotEnd}|
