176bc0b455bbf5233cf4c73a48461a72f9457002
Daniel Leitner <d.leitner@fz-juelich.de>
2025-05-09 09:00:35 +0200

    maybe a bug

--- tutorial/chapter8_implementation/stem_test.py
+++ tutorial/chapter8_implementation/stem_test.py
@@ -1,50 +1,50 @@
-import sys;sys.path.append("../..");sys.path.append("../../src/")  
+import sys;sys.path.append("../..");sys.path.append("../../src/")
 
 import numpy as np
 
-import plantbox as pb  
-import visualisation.vtk_plot as vp  
+import plantbox as pb
+import visualisation.vtk_plot as vp
 
-plant = pb.Plant()  
+plant = pb.Plant()
 
-test = pb.OrganRandomParameter(plant)
-print(test)
-ss
-path = "../../modelparameter/structural/plant/" # Open plant and root parameter from a file
+# test = pb.OrganRandomParameter(plant)
+# print(test)
+
+path = "../../modelparameter/structural/plant/"  # Open plant and root parameter from a file
 name = "fspm2023"
-plant.readParameters(path + name + ".xml")  
+plant.readParameters(path + name + ".xml")
 
 srp = plant.getOrganRandomParameter(pb.seed)
 rrp = plant.getOrganRandomParameter(pb.root)
 strp = plant.getOrganRandomParameter(pb.stem)
 lrp = plant.getOrganRandomParameter(pb.leaf)
 
-print(srp[0])
+# print(srp[0])
 srp[0].firstSB = 1.e6
 srp[0].firstB = 1.e6
 srp[0].firstTil = 1.e6
-
-dd
 
 # print(rrp[1])
 # print(rrp[1].successor)
 rrp[1].successor = [[]]
 rrp[1].successorP = [[]]
 
-
-
-for i, s in enumerate(strp[1:]):
-    print("***", i)
-    print(s)
-    print()
+# for i, s in enumerate(strp[1:]):
+#     print("***", i)
+#     print(s)
+#     print()
 
 # strp[0].successorOT = [[]]
 # strp[0].successor = [[]]
 # strp[0].successorP = [[]]
 # strp[0].successorNo = []
 
-print(strp[1].lmax)
-strp[1].lmax = 12  # BUG lmax = 10
+print(strp[1])
+strp[1].ln = 4  # BUG? 4
+strp[1].lmax = 12  # BUG? lmax = 10
+strp[1].RotBeta = 0
+strp[1].BetaDev = 0
+strp[1].InitBeta = 0
 
 # strp[1].successorOT = [[]]
 # strp[1].successor = [[]]
@@ -52,6 +52,9 @@ strp[1].lmax = 12  # BUG lmax = 10
 # strp[1].successorNo = [1, 1]
 
 strp[2].theta = np.pi / 2
+# strp[2].RotBeta = 0
+# strp[2].BetaDev = 0
+# strp[2].InitBeta = 0
 # strp[2].successorOT = [[]]
 # strp[2].successor = [[]]
 # strp[2].successorP = [[]]
@@ -59,7 +62,7 @@ strp[2].theta = np.pi / 2
 
 lrp[1].dx = 0.05
 
-plant.writeParameters(name + "_modified.xml")  
+plant.writeParameters(name + "_modified.xml")
 
 plant.initialize()  # Initialize |\label{l13:initialize}|
 simtime = 80  # days
