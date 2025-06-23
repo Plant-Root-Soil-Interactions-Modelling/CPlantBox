2a56840b76aa292266739ba97310687392692caa
Daniel Leitner <d.leitner@fz-juelich.de>
2025-06-23 13:04:52 +0200

    branching probability changed to emergence rate

--- src/structural/Stem.cpp
+++ src/structural/Stem.cpp
@@ -133,7 +133,7 @@ void Stem::simulate(double dt, bool verbose)
 			//use relative coordinates for this function. Delete as it s not a root?
 			double P = getStemRandomParameter()->f_sbp->getValue(nodes.back(),shared_from_this());
 			if (P<1.) { // P==1 means the lateral emerges with probability 1 (default case)
-				double p = 1.-std::pow((1.-P), dt); //probability of emergence in this time step
+				double p = 1.-(1.-P*dt); //probability of emergence in this time step
 				if (plant.lock()->rand()>p) { // not rand()<p
 					age -= dt; // the root does not emerge in this time step
 				}
