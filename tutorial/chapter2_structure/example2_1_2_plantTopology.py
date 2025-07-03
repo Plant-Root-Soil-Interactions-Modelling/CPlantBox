c7d47085fa8ef0f6464f6eedc9dfeef7b165ba7c
m-giraud <m.giraud@fz-juelich.de>
2025-07-03 07:59:59 +0200

    update tutorial

--- tutorial/chapter2_structure/example2_1_2_plantTopology.py
+++ tutorial/chapter2_structure/example2_1_2_plantTopology.py
@@ -1,24 +1,24 @@
-import sys; sys.path.append("../.."); sys.path.append("../../src/") # |\label{l2_1:importStart}|
+import sys; sys.path.append("../..")
+sys.path.append("../../src/") # |\label{l2_1:importStart}|
 path = "../../modelparameter/structural/plant/"
 sys.path.append(path)
 import plantbox as pb
-import visualisation.vtk_plot as vp
-from example1f import template_text # |\label{l2_1:importEnd}|
+import visualisation.vtk_plot as vp # |\label{l2_1:importEnd}|
 
 
-###Simple example
+### Simple example
 p = pb.MappedPlant(2)
-p.readParameters(path + "example2_1.xml", verbose = True)
+p.readParameters(path + "example2_1_2.xml", verbose = True)
 
 rrp = p.getOrganRandomParameter(pb.root)[1] # define laterals of taproot #|\label{l2_1:arrayStart1}|
-rrp.successorOT = [[2]] 
+rrp.successorOT = [[pb.root]] 
 rrp.successorST = [[2]] 
 rrp.successorP  = [[1.0]] 
 rrp.successorNo = [1] 
 rrp.successorWhere  = [[]] #|\label{l2_1:emptywhere}|
 
 srp = p.getOrganRandomParameter(pb.stem)[1] # define laterals of stem
-srp.successorOT = [[3]] 
+srp.successorOT = [[pb.stem]] 
 srp.successorST = [[2]] 
 srp.successorP  = [[1.0]] 
 srp.successorNo = [1] 
@@ -28,53 +28,53 @@ p.initialize(False)
 time = 100
 p.simulate(time, False)
 vp.plot_plant(p, "organType")
-p.write("results/example2_1a.vtp")
+p.write("results/example2_1_2a.vtp")
 
-###Several successor types, specific locations    
+### Several successor types, specific locations    
 p = pb.MappedPlant(2)
-p.readParameters(path + "example2_1.xml")
+p.readParameters(path + "example2_1_2.xml")
 
 rrp = p.getOrganRandomParameter(pb.root)[1] #|\label{l2_1:arrayStart2}|
-rrp.successorOT     = [[2], [4], [3]] 
-rrp.successorST     = [[2], [1], [2]] 
-rrp.successorNo     = [4, 1, 6] 
-rrp.successorP      = [[1.0], [1.0], [1.0]] 
-rrp.successorWhere  = [[-1.0, -3.0, -5.0, -7.0], [], []]
+rrp.successorOT     = [[pb.root], [pb.root]] 
+rrp.successorST     = [[2], [3]] 
+rrp.successorNo     = [1, 20] 
+rrp.successorP      = [[1.0], [1.0]] 
+rrp.successorWhere  = [[-1, -2, -3,-4, -5, -7], [7]]
 
 srp = p.getOrganRandomParameter(pb.stem)[1]
-srp.successorOT     = [[2], [4]] 
-srp.successorST     = [[1], [1]] 
+srp.successorOT     = [[pb.stem], [pb.leaf]] 
+srp.successorST     = [[2], [1]] 
 srp.successorNo     = [1, 4] 
 srp.successorP      = [[1.0], [1.0]] 
-srp.successorWhere  = [[4.0, 6.0, 8.0], [-3.0]] #|\label{l2_1:arrayEnd2}|
+srp.successorWhere  = [[4, 6, 8], [-3]] #|\label{l2_1:arrayEnd2}|
 
 
 p.initialize(False)
 time = 100
 p.simulate(time, False)
 vp.plot_plant(p, "organType")
-p.write("results/example2_1b.vtp")
+p.write("results/example2_1_2b.vtp")
 
-###Probabilistic branching    
+### Probabilistic branching    
 p = pb.MappedPlant(2)
-p.readParameters(path + "example2_1.xml")
+p.readParameters(path + "example2_1_2.xml")
 
 rrp = p.getOrganRandomParameter(pb.root)[1] #|\label{l2_1:arrayStart3}|
-rrp.successorOT     = [[2]] 
+rrp.successorOT     = [[pb.root]] 
 rrp.successorST     = [[2]] 
-rrp.successorP      = [[1.0]] 
+rrp.successorP      = [[0.7]] 
 rrp.successorNo     = [1] 
-rrp.successorWhere  = [[3.0, 5.0]]
+rrp.successorWhere  = []
 
 srp = p.getOrganRandomParameter(pb.stem)[1] 
-srp.successorOT     = [[3, 4, 2], [3]] 
-srp.successorST     = [[2, 1, 3], [2]] 
-srp.successorP      = [[0.2, 0.3, 0.3], [1.0]] 
-srp.successorNo     = [4, 1] 
-srp.successorWhere  = [[-3.0], [3.0]] #|\label{l2_1:arrayEnd3}|
+srp.successorOT     = [[pb.stem, pb.leaf], [pb.stem]] 
+srp.successorST     = [[2, 1], [2]] 
+srp.successorP      = [[0.2, 0.8], [1.0]] 
+srp.successorNo     = [4, 10] 
+srp.successorWhere  = [[-3], [3]] #|\label{l2_1:arrayEnd3}|
 
 p.initialize(False)
 time = 100
 p.simulate(time, False)
 vp.plot_plant(p, "organType")
-p.write("results/example2_1c.vtp")
+p.write("results/example2_1_2c.vtp")
