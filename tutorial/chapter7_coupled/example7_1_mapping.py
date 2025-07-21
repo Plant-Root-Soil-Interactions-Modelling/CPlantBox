8fbb7a4a84e68ebf16c7d208473045e0daf44a2e
Daniel Leitner <d.leitner@fz-juelich.de>
2025-07-21 14:56:56 +0200

    ongoing

--- tutorial/chapter7_coupled/example7_1_mapping.py
+++ tutorial/chapter7_coupled/example7_1_mapping.py
@@ -12,18 +12,19 @@ import numpy as np
 """ Root system """  # |\label{l71m:root_system_start}|
 plant = pb.MappedPlant()
 path = "../../modelparameter/structural/rootsystem/"
-name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010
+name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010, Anagallis_femina_Leitner_2010
 plant.readParameters(path + name + ".xml")
+plant.setSeed(4)
 plant.initialize()  # |\label{l71m:root_system_end}|
 
 """ Macroscopic soil grid """  # |\label{l71m:grid_start}|
 min_b = np.array([-2, -2, -15])  # [cm]
 max_b = np.array([2, 2, -5])  # [cm]
-cell_number = np.array([5, 3, 12])  # [1]
-periodic = True
+cell_number = np.array([2, 3, 6])  # [1]
 s = RichardsWrapper(RichardsSP())
 s.initialize()
-s.createGrid(min_b, max_b, cell_number, periodic)  # TODO periodicity seems to not work for [1,X,X]
+periodic = True
+s.createGrid(min_b, max_b, cell_number, periodic)
 s.setVGParameters([[0.08, 0.43, 0.04, 1.6, 50]])
 s.setHomogeneousIC(-300, True)  # cm pressure head, equilibrium
 s.setTopBC("noFlux")
@@ -31,24 +32,24 @@ s.setBotBC("noFlux")
 s.initializeProblem()  # |\label{l71m:grid_end}|
 
 """ Coupling """
-picker = lambda x, y, z: s.pick([x, y, z])
-plant.setSoilGrid(picker)
+picker = lambda x, y, z: s.pick([x, y, z])  # |\label{l71m:picker}|
+plant.setSoilGrid(picker)  # |\label{l71m:picker_end}|
 
 """ Simulate """
-plant.simulate(10., False)
-
-""" Find grid cell index for segment """
-segs = plant.segments  # |\label{l71m:segments}|
-x = np.array([plant.seg2cell[i] for i in range(0, len(segs))])  # |\label{l71m:seg2cell}|
+plant.simulate(10., False)  # |\label{l71m:simulate}|
 
 """ Find segment indices in a grid cell"""
-ci = plant.soil_index(0, 0, -7)  # grid cell index
+ci = plant.soil_index(0, 0, -7)  # grid cell index |\label{l71m:soil_index}|
 print("Cell at [0,0,-7] has index", ci)
 try:
     print(len(plant.cell2seg[ci]), "segments in this cell:")  # |\label{l71m:cell2seg}|
     print(plant.cell2seg[ci])
 except:
     print("There are no segments in this cell")
+
+""" Find grid cell index for segment """
+segs = plant.segments  # |\label{l71m:segments}|
+x = np.array([plant.seg2cell[i] for i in range(0, len(segs))])  # |\label{l71m:seg2cell}|
 
 """ Visualize"""  # |\label{l71m:visualize}|
 ana = pb.SegmentAnalyser(plant.mappedSegments())  # |\label{l71m:mappedSegments}|
--- tutorial/chapter7_coupled/example7_2_coupling.py
+++ tutorial/chapter7_coupled/example7_2_coupling.py
@@ -5,11 +5,11 @@ sys.path.append("../../../dumux-rosi/python/modules/")  # python wrappers
 
 import plantbox as pb
 import visualisation.vtk_plot as vp
-from functional.xylem_flux import XylemFluxPython  # Python hybrid solver
-from functional.root_conductivities import *  # hard coded conductivities
+from functional.PlantHydraulicParameters import PlantHydraulicParameters  # |\label{l41:imports}|
+from functional.PlantHydraulicModel import HydraulicModel_Doussan
+from functional.PlantHydraulicModel import HydraulicModel_Meunier  # |\label{l41:imports_end}|
 from rosi_richards import RichardsSP  # C++ part (Dumux binding)
 from richards import RichardsWrapper  # Python part
-
 import numpy as np
 import matplotlib.pyplot as plt
 import timeit
@@ -20,86 +20,94 @@ def sinusoidal(t):
 
 
 """ Parameters """
+# min_b = [-38., -8., -50.]  # [cm]
+# max_b = [38., 8., 0.]  # [cm]
+# cell_number = [19, 3, 50]  # [1] spatial resolution (1D model)
+# cell_number = [1, 1, 50]
+
 min_b = [-4., -4., -25.]
 max_b = [4., 4., 0.]
 cell_number = [8, 8, 25]  # [16, 16, 30]  # [32, 32, 60]
-periodic = False
 
 path = "../../modelparameter/structural/rootsystem/"
-name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010
+name = "Anagallis_femina_Leitner_2010"  # Zea_mays_1_Leitner_2010, Anagallis_femina_Leitner_2010
 loam = [0.08, 0.43, 0.04, 1.6, 50]
-initial = -659.8 + 12.5  # -659.8
+initial = -600  # cm
 
-trans = 6.4  # cm3 /day (sinusoidal)
+trans = 6.4  # cm3 /day (sinusoidal) = mL/day
 wilting_point = -10000  # cm
 
 sim_time = 7  # [day] for task b
 rs_age = 10  # root system initial age
-age_dependent = False  # conductivities
 dt = 360. / (24 * 3600)  # [days] Time step must be very small
 
 """ Initialize macroscopic soil model """
 s = RichardsWrapper(RichardsSP())
 s.initialize()
-s.createGrid(min_b, max_b, cell_number, periodic)  # [cm]
+s.createGrid(min_b, max_b, cell_number, True)  # [cm]
 s.setHomogeneousIC(initial, True)  # cm pressure head, equilibrium
 s.setTopBC("noFlux")
 s.setBotBC("noFlux")
 s.setVGParameters([loam])
+# s.setParameter("Newton.EnableChop", "True")
+# s.setParameter("Newton.EnableAbsoluteResidualCriterion", "True")
+s.setParameter("Soil.SourceSlope", "100")
 s.initializeProblem()
 s.setCriticalPressure(wilting_point)
 
 """ Initialize xylem model """
-rs = pb.MappedPlant()
-rs.readParameters(path + name + ".xml")
-if not periodic:
-    sdf = pb.SDF_PlantBox(0.99 * (max_b[0] - min_b[0]), 0.99 * (max_b[1] - min_b[1]), max_b[2] - min_b[2])
-else:
-    sdf = pb.SDF_PlantBox(np.Inf, np.Inf, max_b[2] - min_b[2])
-rs.setGeometry(sdf)
-r = XylemFluxPython(rs)
-init_conductivities(r, age_dependent)
+plant = pb.MappedPlant()
+plant.readParameters(path + name + ".xml")
+sdf = pb.SDF_PlantBox(np.inf, np.inf, max_b[2] - min_b[2])
+plant.setGeometry(sdf)
+
+""" root hydraulic properties """
+params = PlantHydraulicParameters()  # |\label{l41:hydraulicparams}|
+# params.read_parameters("../../modelparameter/functional/plant_hydraulics/couvreur2012")
+kz = 4.32e-2  # [cm^3/day]
+kr = 1.728e-4  # [1/day]
+params.set_kr_const(kr)
+params.set_kx_const(kz)
+# params.plot_conductivities(True)  # for maize
+hm = HydraulicModel_Doussan(plant, params)
 
 """ Coupling (map indices) """
 picker = lambda x, y, z: s.pick([x, y, z])
-r.rs.setSoilGrid(picker)  # maps segments
-r.rs.setRectangularGrid(pb.Vector3d(min_b), pb.Vector3d(max_b), pb.Vector3d(cell_number), True)
-rs.initialize()
-rs.simulate(rs_age, False)
-r.test()  # sanity checks
-nodes = r.get_nodes()
-cci = picker(nodes[0, 0], nodes[0, 1], nodes[0, 2])  # collar cell index
+plant.setSoilGrid(picker)  # maps segments
+plant.setRectangularGrid(pb.Vector3d(min_b), pb.Vector3d(max_b), pb.Vector3d(cell_number), True)
+plant.initialize()
+plant.simulate(rs_age, False)
+hm.test()  # sanity checks
 
 """ Numerical solution """
 start_time = timeit.default_timer()
 x_, y_ = [], []
-sx = s.getSolutionHead()  # inital condition, solverbase.py
+sx = s.getSolutionHead()  # initial condition, solverbase.py
 N = round(sim_time / dt)
 t = 0.
 
 for i in range(0, N):
 
-    rs.simulate(dt)
+    plant.simulate(dt)
+    rx = hm.solve(rs_age + t, -trans * sinusoidal(t), sx, cells = True)
 
-    rx = r.solve(rs_age + t, -trans * sinusoidal(t), sx[cci], sx, True, wilting_point)  # xylem_flux.py
     x_.append(t)
-    y_.append(float(r.collar_flux(rs_age + t, rx, sx)))  # exact root collar flux
+    y_.append(float(hm.get_transpiration(rs_age + t, rx, sx, cells = True)))  # exact root collar flux
 
-    fluxes = r.soilFluxes(rs_age + t, rx, sx, False)
-    s.setSource(fluxes)  # richards.py
+    fluxes = hm.soil_fluxes(rs_age + t, rx, sx)
+    s.setSource(fluxes)
     s.solve(dt)
-    sx = s.getSolutionHead()  # richards.py
+    sx = s.getSolutionHead()
 
-    min_sx, min_rx, max_sx, max_rx = np.min(sx), np.min(rx), np.max(sx), np.max(rx)
     n = round(float(i) / float(N) * 100.)
     print("[" + ''.join(["*"]) * n + ''.join([" "]) * (100 - n) + "], [{:g}, {:g}] cm soil [{:g}, {:g}] cm root at {:g} days {:g}"
-            .format(min_sx, max_sx, min_rx, max_rx, s.simTime, rx[0]))
+            .format(np.min(sx), np.max(sx), np.min(rx), np.max(rx), s.simTime, rx[0]))
     t += dt
 
 print ("Coupled benchmark solved in ", timeit.default_timer() - start_time, " s")
 
 """ VTK visualisation """
-vp.plot_roots_and_soil(r.rs, "pressure head", rx, s, periodic, np.array(min_b), np.array(max_b), cell_number, name)
+vp.plot_roots_and_soil(hm.ms, "pressure head", rx, s, True, np.array(min_b), np.array(max_b), cell_number, name)
 
 """ transpiration over time """
 fig, ax1 = plt.subplots()
