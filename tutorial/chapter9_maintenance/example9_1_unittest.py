81fbb4c1d01c701675ed91f49e3458b0c35a30e8
Anna Heck <anna.heck@outlook.com>
2025-07-24 13:43:21 +0200

    added stuff that uses self needs work

--- tutorial/chapter9_maintenance/example9_1_unittest.py
+++ tutorial/chapter9_maintenance/example9_1_unittest.py
@@ -13,6 +13,50 @@ def rootLength(t, r, k):  # root length at a certain age
 
 class TestPlant(unittest.TestCase):
 
+    def root_example_rrp(self):
+        """ an example used in the tests below, a main root with laterals """
+        self.plant = pb.Organism()  # store organism (not owned by Organ, or OrganRandomParameter)
+        p0 = pb.RootRandomParameter(self.plant)
+        p0.name, p0.subType, p0.la, p0.lb, p0.lmax, p0.ln, p0.r, p0.dx = "taproot", 1, 10., 1., 100., 1., 1.5, 0.5
+        p0.successor = [[2]]
+        p0.successorP = [[1.]]
+        p1 = pb.RootRandomParameter(self.plant)
+        p1.name, p1.subType, p1.lmax, p1.r, p1.dx = "lateral", 2, 25., 2., 0.1
+        self.p0, self.p1 = p0, p1  # needed at later point
+        self.plant.setOrganRandomParameter(p0)  # the organism manages the type parameters and takes ownership
+        self.plant.setOrganRandomParameter(p1)
+        srp = pb.SeedRandomParameter(self.plant)
+        self.plant.setOrganRandomParameter(srp)
+
+        param0 = p0.realize()  # set up root by hand (without a root system)
+        param0.la, param0.lb = 0, 0  # its important parent has zero length, otherwise creation times are messed up
+        parentroot = pb.Root(1, param0, True, True, 0., 0., pb.Vector3d(0, 0, -1), 0, False, 0)  # takes ownership of param0
+        parentroot.setOrganism(self.plant)
+        parentroot.addNode(pb.Vector3d(0, 0, -3), 0)  # there is no nullptr in Python
+
+        self.parentroot = parentroot  # store parent (not owned by child Organ)
+        self.root = pb.Root(self.plant, p0.subType,  0, self.parentroot , 0)
+        self.root.setOrganism(self.plant)
+
+    def test_constructors(self):
+        """ tests two kinds of constructors and copy"""
+        self.root_example_rrp()
+        # 1. constructor from scratch
+        param = self.p0.realize()
+        root = pb.Root(1, param, True, True, 0., 0., pb.Vector3d(0, 0, -1), 0, False, 0)
+        root.setOrganism(self.plant)
+        root.addNode(pb.Vector3d(0, 0, -3), 0)  # parent must have at least one nodes
+        # 2. used in simulation (must have parent, since there is no nullptr in Pyhton)
+        root2 = pb.Root(self.plant, self.p1.subType, 0, root, 0)
+        root.addChild(root2)
+        # 3. deep copy (with a factory function)
+        plant2 = pb.Organism()
+        root3 = root.copy(plant2)
+        self.assertEqual(str(root), str(root3), "deep copy: the root string representations shold be equal")
+        self.assertIsNot(root.getParam(), root3.getParam(), "deep copy: roots have same specific parameter set")  # type OrganSpecificParameter
+        self.assertEqual(str(root.param()), str(root3.param()), "deep copy: roots have different parameter values")  # type RootSpecificParameter
+
+
     def test_copy(self):
         """ checks if the root system can be copied, and if randomness works """
         seed = 110  # random seed
@@ -37,131 +81,6 @@ class TestPlant(unittest.TestCase):
         self.assertEqual(rs3.rand(), n1, "copy: random generator seed was not copied")
         rs3.simulate(10)
         self.assertEqual(rs3.rand(), n2, "copy: simulation is not deterministic")
-
-    def test_CPlantBox(self):
-        """tests the functions needed by CPlantBox defined in CPlantBox_PiafMunch.py"""
-        p = pb.Plant()
-        p.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-
-        seeds = p.getOrganRandomParameter(pb.OrganTypes.seed)
-        roots = p.getOrganRandomParameter(pb.OrganTypes.root)
-        stems = p.getOrganRandomParameter(pb.OrganTypes.stem)
-        leafs = p.getOrganRandomParameter(pb.OrganTypes.leaf)
-#         for p_ in seeds:
-#             print(p_)
-#         for p_ in roots[1:]:
-#             print(p_)
-#         for p_ in stems[1:]:
-#             print(p_)
-#         for p_ in leafs[1:]:
-#             print(p_)
-
-        self.assertEqual([len(seeds), len(roots[1:]), len(stems[1:]), len(leafs[1:])], [1, 3, 3, 1],
-                         "test_CPlantBox: read wrong number of random parameter from xml")
-
-        p.initialize(False)
-        p.simulate(76, False)
-        p.write("morningglory.vtp")
-
-    def test_CPlantBox_analysis(self):
-        """tests the functions needed by CPlantBox_analysis defined in CPlantBox_PiafMunch.py"""
-        p = pb.Plant()
-        p.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-        p.initialize(False)
-        p.simulate(76)
-        ana = pb.SegmentAnalyser(p)
-        ana.write("morningglory_ama.vtp")
-
-    def test_convert(self):
-        """tests the functions needed by the convert function of CPlantBox_PiafMunch.py"""
-        p = pb.Plant()
-        p.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-        p.initialize(False)
-        p.simulate(76)
-        nodes = np.array([np.array(a) / 100 for a in p.getNodes()])  # convert to numpy array, and from cm to m
-        # print(nodes.shape)
-        rseg = np.array([np.array(s) for s in p.getSegments(pb.OrganTypes.root)])  # root system segments
-        # print(rseg.shape)
-        sseg = np.array([np.array(s) for s in p.getSegments(pb.OrganTypes.stem)])  # stem system segments
-        # print(sseg.shape)
-#         lseg = v2ai(plant.getNodesOrganType())
-        l = np.array([ o.getParameter("organType") for o in p.getSegmentOrigins()])
-        # print(l.shape)
-#         plant_ana = pb.SegmentAnalyser(p)
-#         node_connection_o = seg2a(p.getSegments(15)) # plant segments
-        pass
-
-    def test_CPlantBox_step(self):
-        """tests the functions needed by CPlantBox defined in CPlantBox_PiafMunch.py"""
-        p1 = pb.MappedPlant(2)
-        p1.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-        p1.initialize(verbose = False, stochastic = False)
-        p1.simulate(76, False)
-        p1.write("test_CPlantBox_1step.vtp")
-
-        p2 = pb.MappedPlant(2)
-        p2.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-        p2.initialize(verbose = False, stochastic = False)
-        for i in range(100):
-            p2.simulate(76 / 100, False)
-        p2.write("test_CPlantBox_100step.vtp")
-        root1 = p1.getOrgans(2)
-        root2 = p2.getOrgans(2)
-        self.assertAlmostEqual(len(root1), len(root2), 10, "number of root organs do not agree")
-        root1 = p1.getOrgans(3)
-        root2 = p2.getOrgans(3)
-        self.assertAlmostEqual(len(root1), len(root2), 10, "number of stem organs do not agree")
-        root1 = p1.getOrgans(4)
-        root2 = p2.getOrgans(4)
-        self.assertAlmostEqual(len(root1), len(root2), 10, "number of leaf organs do not agree")
-
-    def test_DB_delay(self):
-        p = pb.MappedPlant(2)
-        p.readParameters(path + "Heliantus_Pagès_2013.xml", fromFile = True, verbose = False)
-
-        p.initializeDB()
-        time = 76
-
-        rrp = p.getOrganRandomParameter(pb.root)[1]
-        rrp.ldelay = 3
-        rrp.ldelays = 0.
-        # print(rrp)
-        p.setOrganRandomParameter(rrp)
-
-        rrp = p.getOrganRandomParameter(pb.root)[2]
-        rrp.ldelay = 5
-        rrp.ldelays = 0.
-        # print(rrp)
-        p.setOrganRandomParameter(rrp)
-
-        rrp = p.getOrganRandomParameter(pb.root)[3]
-        rrp.ldelay = 5
-        rrp.ldelays = 0.
-        # print(rrp)
-        p.setOrganRandomParameter(rrp)
-        tl, rl = [], []
-        p.simulate(time, False)
-        for i, r in enumerate(p.getOrgans(pb.root)):
-            rl.append(r.getLength())
-            et, dl = 0, 0  # no delay for basal roots
-            rsp = r.getParam()
-            if rsp.subType > 1:
-                dl = r.getParent().getOrganRandomParameter().ldelay  # only works because deviation == 0
-                et = r.getParent().getNodeCT(r.parentNI) + dl
-            self.assertAlmostEqual(r.getAge(), (time - et), 10, "numeric and analytic age of root n#" + str(i + 1) + " do not agree")
-
-    def test_missing_laterals(self):
-        """can CPB handle it when no lateral appear at a branching point
-            because of probabilistic branching?
-            currently, just tests that cpb does not crash
-        """
-        p = pb.MappedPlant(2)
-        p.readParameters(path + "test_missing_laterals.xml", fromFile = True, verbose = False)
-        p.initialize(False)
-        time = 76
-        p.simulate(time, False)
-        p.write("test_missing_laterals.vtp")
-
 
 if __name__ == '__main__':
     # MANY tests missing !!!
