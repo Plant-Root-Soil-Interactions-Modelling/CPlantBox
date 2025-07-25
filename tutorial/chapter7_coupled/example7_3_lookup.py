cdcc61fe87686a674bb33dbb16b555a1cab8edf7
Daniel Leitner <d.leitner@fz-juelich.de>
2025-07-25 15:11:05 +0200

    lookup table added

---
+++ tutorial/chapter7_coupled/example7_3_lookup.py
@@ -0,0 +1,14 @@
+""" how to create a look up table for nonlinear perirhzal resistances, 
+    run on mulitple threads using: mpiexec -n 4 python3 example7_3_lookup.py """
+import sys; sys.path.append("../.."); sys.path.append("../../src/")
+
+from functional.Perirhizal import PerirhizalPython
+import functional.van_genuchten as vg
+
+peri = PerirhizalPython()
+hydrus_loam = [0.078, 0.43, 0.036, 1.56, 24.96]
+filename = "hydrus_loam"
+sp = vg.Parameters(hydrus_loam)
+vg.create_mfp_lookup(sp)
+peri.create_lookup_mpi(filename, sp)
+# peri.open_lookup(filename)
