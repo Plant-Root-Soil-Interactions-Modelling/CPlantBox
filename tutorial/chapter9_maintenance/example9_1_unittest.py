1ae49eda263991940a98074775beaeecfea20179
Anna Heck <anna.heck@outlook.com>
2025-07-28 11:44:50 +0200

    brought files upto date and created unit test example

--- tutorial/chapter9_maintenance/example9_1_unittest.py
+++ tutorial/chapter9_maintenance/example9_1_unittest.py
@@ -1,15 +1,8 @@
-import sys; sys.path.append(".."); sys.path.append("../src/")
+import sys; sys.path.append("../.."); sys.path.append("../../src/")
 import unittest
 
 import plantbox as pb
 from rsml.rsml_reader import *
-
-path = "../modelparameter/structural/plant/"
-
-
-def rootLength(t, r, k):  # root length at a certain age
-    return k * (1 - np.exp(-r * t / k))
-
 
 class TestPlant(unittest.TestCase):
 
@@ -38,51 +31,33 @@ class TestPlant(unittest.TestCase):
         self.root = pb.Root(self.plant, p0.subType,  0, self.parentroot , 0)
         self.root.setOrganism(self.plant)
 
-    def test_constructors(self):
-        """ tests two kinds of constructors and copy"""
+    def test_parameter(self):
+        """ tests some parameters on sequential organ list """
         self.root_example_rrp()
-        # 1. constructor from scratch
-        param = self.p0.realize()
-        root = pb.Root(1, param, True, True, 0., 0., pb.Vector3d(0, 0, -1), 0, False, 0)
-        root.setOrganism(self.plant)
-        root.addNode(pb.Vector3d(0, 0, -3), 0)  # parent must have at least one nodes
-        # 2. used in simulation (must have parent, since there is no nullptr in Pyhton)
-        root2 = pb.Root(self.plant, self.p1.subType, 0, root, 0)
-        root.addChild(root2)
-        # 3. deep copy (with a factory function)
-        plant2 = pb.Organism()
-        root3 = root.copy(plant2)
-        self.assertEqual(str(root), str(root3), "deep copy: the root string representations shold be equal")
-        self.assertIsNot(root.getParam(), root3.getParam(), "deep copy: roots have same specific parameter set")  # type OrganSpecificParameter
-        self.assertEqual(str(root.param()), str(root3.param()), "deep copy: roots have different parameter values")  # type RootSpecificParameter
+        simtime = 30.
+        self.root.simulate(simtime, False)
+        organs = self.root.getOrgans()
+        age, ct = [], []
+        for o in organs:
+            age.append(o.getParameter("age"))
+            ct.append(o.getParameter("creationTime"))
 
-
+        nol = round(self.root.getParameter("numberOfLaterals"))
+        for i in range(0, nol):
+            self.assertAlmostEqual(age[i], simtime - ct[i], 10, "getParameter: age and creation time does not agree")
+    
     def test_copy(self):
         """ checks if the root system can be copied, and if randomness works """
         seed = 110  # random seed
+        path = "../../modelparameter/structural/plant/"
         name = "Brassica_oleracea_Vansteenkiste_2014"
         rs = pb.Plant()  # the original
-        rs.readParameters("../modelparameter/structural/rootsystem/" + name + ".xml", verbose = False)
+        rs.readParameters(path + name + ".xml", verbose = False)
         rs.setSeed(seed)
         rs.initialize(False)
         rs2 = rs.copy()  # copy root system
-        n1 = rs.rand()
         self.assertIsNot(rs2, rs, "copy: not a copy")
         self.assertEqual(str(rs), str(rs2), "copy: the organisms should be equal")
-        self.assertEqual(rs2.rand(), n1, "copy: random generator seed was not copied")
-        rs.simulate(10)
-        rs2.simulate(10)
-        n2 = rs.rand()
-        self.assertEqual(rs2.rand(), n2, "copy: simulation is not deterministic")
-        rs3 = pb.Plant()  # rebuild same
-        rs3.readParameters("../modelparameter/structural/rootsystem/" + name + ".xml", verbose = False)
-        rs3.setSeed(seed)
-        rs3.initialize(False)
-        self.assertEqual(rs3.rand(), n1, "copy: random generator seed was not copied")
-        rs3.simulate(10)
-        self.assertEqual(rs3.rand(), n2, "copy: simulation is not deterministic")
 
 if __name__ == '__main__':
-    # MANY tests missing !!!
-
     unittest.main()
