b1bcca11b5e09ffe2b2145a5b641ab164b531f53
hpagel <h.pagel@fz-juelich.de>
2025-08-19 16:40:46 +0200

    small corrections, generalisation of system folder addition and adaption of simulation scenario

--- tutorial/chapter6_soil/example6_1_infiltration_evaporation_Selhausen.py
+++ tutorial/chapter6_soil/example6_1_infiltration_evaporation_Selhausen.py
@@ -1,15 +1,16 @@
-# This example builds on the infiltration and evaporation problem M2.1 and M2.2 from Schnepf et al. (2023, \url{doi.org/10.1093/insilicoplants/diad005}) and extends the simulation to a multi-layered soill profile at the Field Minirhizotron Facilities Selhausen (\url{https://www.fz-juelich.de/en/ibg/ibg-3/research-groups/modelling-terrestrial-systems/soil-root-systems-and-rhizosphere-processes/field-minirhizotron-facilities}). 
+# This example builds on the infiltration and evaporation problem M2.1 and M2.2 from Schnepf et al. (2023, \url{doi.org/10.1093/insilicoplants/diad005}) and extends the simulation to a multi-layered soil profile at the Field Minirhizotron Facilities Selhausen (\url{https://www.fz-juelich.de/en/ibg/ibg-3/research-groups/modelling-terrestrial-systems/soil-root-systems-and-rhizosphere-processes/field-minirhizotron-facilities}). 
 # The hydraulic properties of the soil profile are taken from Bauer et al. (2011, table 3, \url{https://doi.org/10.1007/s10533-011-9583-1}). 
-# The Richards equations is solved to simulate water infiltration over 1 day into an initially dry soil followed by evaporation over 1 day. Only the vertical water movement is considered. A Neumann boundary condition is set at the upper boundary and a free drainage boundary condition is set at the lower boundary. 
+# The Richards equations is solved to simulate water infiltration over 1 day into an initially dry soil followed by evaporation over 2 days. Only the vertical water movement is considered. A Neumann boundary condition is set at the upper boundary and a free drainage boundary condition is set at the lower boundary. 
 
 # The code solves the Richards equation with DuMux. The github repository "dumux-rosi" (https://github.com/Plant-Root-Soil-Interactions-Modelling/dumux-rosi.git) needs to be cloned
 # into the same path level as CPlantBox.
 
 # add paths to the folders containing CPlantBox and dumux-rosi
-import sys;
-sys.path.append("../../../dumux-rosi/python/modules");
-sys.path.append("../../../dumux-rosi/build-cmake/cpp/python_binding/");
-sys.path.append("../../../CPlantBox/src")
+import sys
+base_path= "./CPlantBox/dumux/"
+sys.path.append(base_path+"dumux-rosi/python/modules")
+sys.path.append(base_path+"dumux-rosi/build-cmake/cpp/python_binding/")
+sys.path.append(base_path+"CPlantBox/src")
 
 import matplotlib.pyplot as plt  #
 import numpy as np  #
@@ -17,20 +18,20 @@ import numpy as np  #
 from rosi_richards import RichardsSPnum  # C++ part (Dumux binding) 
 from richards import RichardsWrapper  # Python part  
 
-# Define Mualem van Genuchten parameters for Selhausen soil profile accordinf to Bauer et al. (2011, table 3, \url{https://doi.org/10.1007/s10533-011-9583-1}) |\label{l61ies:genuchten_a}|
+# Define Mualem van Genuchten parameters for Selhausen soil profile according to Bauer et al. (2011, table 3, \url{https://doi.org/10.1007/s10533-011-9583-1}) |\label{l61ies:genuchten_a}|
 # theta_r (-), theta_s (-), alpha (1/cm), n (-), Ks (cm d-1) 
 l1 = [0.008, 0.389, 0.012, 1.97, 91.68] # 0-20 cm
 l2 = [0.008, 0.389, 0.023, 1.23, 63.36] # 20-33 cm
 l3 = [0.008, 0.389, 0.01, 1.1, 10] # 33-57 cm
 l4 = [0.008, 0.389, 0.01, 1.1, 10] # 57-120 cm
 soil = [l1,l2,l3,l4] # Combine the hydraulic conductivity vectors from all soil layers to define soil type for simulation
-sim_time = 2.05  # 
+sim_time = 3.05  # 
 dt = 0.05 #720 / (24 * 3600)  # time step [days]   
 
 # Solve the Richards equation using the Python wrapper of dumux-rosi
 s = RichardsWrapper(RichardsSPnum())  # 
 s.initialize()  #
-s.setTopBC("atmospheric", 0.5, [[0., 1.,1.,2], [10., 10.,-0.1,-0.1]])  #  [cm/day] atmospheric is with surface run-off   |\label{l61ies:top_bc}|
+s.setTopBC("atmospheric", 0.5, [[0., 1.,1.,3.], [10., 10.,-0.1,-0.1]])  #  [cm/day] atmospheric is with surface run-off   |\label{l61ies:top_bc}|
 s.setBotBC("freeDrainage")  # |\label{l61ies:bottom_bc}|
 N = 119*10 # use a fine grid resolution of 1 mm per grid point in z direction |\label{l61ies:grid}|
 s.createGrid([-5., -5., -120.], [5., 5., 0.], [1, 1, N])  # [cm] N   |\label{l61ies:grid}|
@@ -74,7 +75,7 @@ bot_new = np.array(bot_new)
 soil_times = np.array(soil_times)  
 
 # define output times
-sel_idx=np.searchsorted(soil_times, [0., 0.2, 0.5, 1.,1.2, 1.5,2.])
+sel_idx=np.searchsorted(soil_times, [0., 0.2, 0.5, 1.,1.2, 2.,3.])
 
 # Plot solutions |\label{l61ies:plt_prof}|
 fig1, axs = plt.subplots(2,1, sharex='row',figsize=(12,12))
@@ -84,7 +85,7 @@ for i in sel_idx:
     axs[0].plot(x_[i], z_[i], cols[ii],label = f'{soil_times[i]:.2f} d')
     axs[1].plot(h_[i], z_[i], cols[ii],label = f'{soil_times[i]:.2f} d')
     ii=ii+1
-axs[0].set_title("Infiltration 1 $d$ with 10 $cm$ $d^{-1}$ \n and evaporation 1 $d$ with 0.1 $cm$ $d^{-1}$ afterwards")
+axs[0].set_title("Infiltration 1 $d$ with 10 $cm$ $d^{-1}$ \n and evaporation 2 $d$ with 0.1 $cm$ $d^{-1}$ afterwards")
 axs[0].set_xlabel('Volumetric water content [$cm^{3}$ $cm^{-3}$]')
 axs[0].set_ylabel('Depth [cm]')
 axs[1].set_xlabel('Head [cm]')
