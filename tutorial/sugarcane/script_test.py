5d73d48d8bcb0a294918493eb2b06b5cce1fd10e
m-giraud <m.giraud@fz-juelich.de>
2025-09-03 11:26:31 +0200

    make similar to matlab outputs

--- tutorial/sugarcane/script_test.py
+++ tutorial/sugarcane/script_test.py
@@ -17,8 +17,8 @@ import visualisation.vtk_plot as vp
 # Configure simulations
 
 # Simulation steps
-simtime = 150  # [days]
-dt = 1
+simtime = 300.  # [days]
+dt = 300.
 steps = round(simtime / dt)
 
 # Assuming a constant carbon source to the root system (this could be replaced by a dynamic model)
@@ -38,23 +38,23 @@ SRF = 1  # Root reduction factor
 scale_elongation = pb.EquidistantGrid1D(0, -50, 100)  # for root elongation from 0 cm to -50 cm, 100 nodes = 99 layers
 soil_strength =np.ones(len(scale_elongation.data)-1) * SRF # rary soil strength that proportionaly reduce root growth by 10%
 scale_elongation.data = soil_strength  # set proportionality factors
-
+#print('scale_elongation',scale_elongation)
 # Proportionally scale this function
-se = pb.ProportionalElongation()
-se.setBaseLookUp(scale_elongation)
+#se = pb.ProportionalElongation()
+#se.setBaseLookUp(scale_elongation)
 
 # Instantiate root system for a maize plant
-rs = pb.Plant()
+rs = pb.Plant(1)
 #rs = pb.RootSystem()
-rs.setSeed(0)
+# rs.setSeed(0)
 name = "./Sugarcane2025"
 rs.readParameters(name + ".xml")
 
 
 # Set the scaling function and initialize
-for p in rs.getOrganRandomParameter(pb.root):
+#for p in rs.getOrganRandomParameter(pb.root):
 #for p in rs.getRootRandomParameter():
-    p.f_se = se
+#    p.f_se = se
 rs.initialize()
 
 total_root_len = rs.getSummed("length")
@@ -63,14 +63,14 @@ total_root_len = rs.getSummed("length")
 # Simulation loop
 for step in range(0, steps):
 
-    print("\nSimulation step", step)
+    print("\nSimulation step", step, steps)
 
     # Maximal total root length increment (cm/day)
     # carbon_source and root_SRL could be replaced by dynamic models
     maxinc = carbon_source * root_SRL
 
     # Simulate growth considering max root increment
-    if False: #limit_carbon:
+    if limit_carbon:
         rs.simulate(dt, maxinc, se, True)
     else:
         rs.simulate(dt)
@@ -91,10 +91,11 @@ for step in range(0, steps):
     # Carbon balance check
     tol = 1.01 # 1% tolerance
     #assert used_carbon <= carbon_source*tol, f"Mismatching carbon balance, used carbon is larger than carbon source ({round((tol-1)*100,3)}% tolerance)"
-    if step == 0:
-        df_out = hf.asDataFrame(rs, step)
-    else:
-        df_out = pd.concat([df_out,hf.asDataFrame(rs, step)], ignore_index=True)
+    if False:
+        if step == 0:
+            df_out = hf.asDataFrame(rs, step)
+        else:
+            df_out = pd.concat([df_out,hf.asDataFrame(rs, step)], ignore_index=True)
 
 # %%
 # Write outputs and plot
@@ -102,11 +103,11 @@ if not Path("results").exists():
     Path("results").mkdir()
 
 rs.write("results/example_carbon.vtp")
-df_out.to_csv("results/example_carbon.csv", index=False)
+#df_out.to_csv("results/example_carbon.csv", index=False)
 
-df_out.columns
+#df_out.columns
 
-df_out.plot(x="RootSystem_Age_days", y="RootTipDepthMax_cm")
+#df_out.plot(x="RootSystem_Age_days", y="RootTipDepthMax_cm")
 
 ana = pb.SegmentAnalyser()
 ana.addSegments(rs)
