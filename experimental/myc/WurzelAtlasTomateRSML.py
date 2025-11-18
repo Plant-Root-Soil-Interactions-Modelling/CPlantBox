8019f283005cbf2cf6bc58cdf93a780154b509c5
Anna Heck <an.heck@fz-juelich.de>
2025-11-18 15:50:41 +0100

    current state 20251118

--- experimental/myc/WurzelAtlasTomateRSML.py
+++ experimental/myc/WurzelAtlasTomateRSML.py
@@ -8,46 +8,117 @@ import rsml.rsml_reader as rsml
 import numpy as np
 import matplotlib.pyplot as plt
 
+path = ""
+name = "WurzelAtlasParasTomate"
+name_new = "WurzelAtlasTomate_parameters_out"
+name_Johanna_WildType = "TomatoJohanna_WildType"
+name_Johanna_RMC = "TomatoJohanna_RMC"
 
-polylines, props, funcs, metadata = rsml.read_rsml("Wurzelatlas_Tomate.rsml")
-print(len(polylines), "roots")
-# print(props["parent-poly"])
-# print(props["parent-node"])
-print("Taproot number of nodes", len(polylines[0]))
-firstOrderLengths = []
-firstOrderIndices = []
-parentNodeIndices = []
-childNodeIndices = []
-secondOrderLengths = []
-    
-for i in range(1, len(polylines)-1):
-    pn = int(props["parent-node"][i])
-    try:
-        parentNodeIndices.append(pn)
-        childNodeIndices.append(i)
-        if pn == 1:
-            firstOrderLengths.append(props["length"][i])
-            firstOrderIndices.append(i)
-        elif pn > 1 and pn in firstOrderIndices:
-            secondOrderLengths.append(props["length"][i])
-    except:
-        print("root", i, "index out of range", "parent-node", pn, "length", props["length"][i])
-
-plt.hist(parentNodeIndices, bins=200)
-plt.show()
-
-parentNodeIndices = np.unique(parentNodeIndices)
-childNodeIndices = np.unique(childNodeIndices)
+animation = False
+pot = False;
 
 
-NotTapRoots = [int(x) for x in childNodeIndices if x in parentNodeIndices]
-NotTapRoots = np.unique(NotTapRoots)
 
-print("First order laterals:", firstOrderLengths)
-print("Second order laterals:", secondOrderLengths)
-print("parent node indices:", parentNodeIndices)
-# print("child node indices:", childNodeIndices)
-print("Not tap roots (both first and second order laterals):", NotTapRoots)
+tomato_WAT = pb.MycorrhizalPlant()
+tomato_J_WT = pb.MycorrhizalPlant()
+tomato_J_RMC = pb.MycorrhizalPlant()
+
+tomato_WAT.readParameters(path +name_new + ".xml", fromFile = True, verbose = True)
+tomato_J_WT.readParameters(path +name_Johanna_WildType + ".xml", fromFile = True, verbose = True)
+tomato_J_RMC.readParameters(path +name_Johanna_RMC + ".xml", fromFile = True, verbose = True)
+
+if pot:
+    # Pot dimensions 11.7 cm x 11.7 cm x 30 cm
+    pot = pb.SDF_PlantContainer(11.7, 11.7, 30, False)
+    tomato_WAT.setGeometry(pot)
+    tomato_J_WT.setGeometry(pot)
+    tomato_J_RMC.setGeometry(pot) 
+
+# root_paras = pb.HyphaeRandomParameter(tomato_WAT)
+# root_paras.hyphalEmergenceDensity = 0
+# root_paras.highresolution = 0
+# tomato_WAT.setOrganRandomParameter(root_paras)
+
+hyphae_parameter = pb.HyphaeRandomParameter(tomato_WAT)
+hyphae_parameter.subType = 1
+hyphae_parameter.a = 0.01
+hyphae_parameter.dx = 0.01
+tomato_WAT.setOrganRandomParameter(hyphae_parameter)
+
+tomato_WAT.initialize(True)
+tomato_J_WT.initialize(True)
+tomato_J_RMC.initialize(True)
+
+# rootWATParas = pb.MycorrhizalRootRandomParameter(tomato_WAT)
+# rootJWTParas = pb.MycorrhizalRootRandomParameter(tomato_J_WT)
+# rootJRMCParas = pb.MycorrhizalRootRandomParameter(tomato_J_RMC)
+# print(rootWATParas)
+# print(rootJWTParas)
+# print(rootJRMCParas)
 
 
-print("Number of parent nodes:", len(parentNodeIndices), "Number of child nodes that are also parent nodes:", len(NotTapRoots))
+# mycp = pb.MycorrhizalPlant()
+# mycp.readParameters(path +name + ".xml", fromFile = True, verbose = True)
+
+# # mycp.setGeometry(pot)
+# mycp.initialize(True)
+# mycp.writeParameters("WurzelAtlasTomate_parameters_out.xml")
+
+totalduration = 108
+potduration = 73
+simtime = totalduration
+fpd = 1 # one step per day
+N = simtime * fpd
+dt = simtime / N
+
+for i in range(1,N):
+    if i % 18 == 0:
+        print('step',i, '/',N)
+    tomato_WAT.simulate(dt, True)
+    tomato_J_WT.simulate(dt, True)
+    tomato_J_RMC.simulate(dt, True)
+        # mycp.simulate(dt, False)
+print('done')
+
+ana_WAT = pb.SegmentAnalyser(tomato_WAT)
+ana_J_WT = pb.SegmentAnalyser(tomato_J_WT)
+ana_J_RMC = pb.SegmentAnalyser(tomato_J_RMC)
+
+rad_WAT = ana_WAT.getParameter("radius")
+rad_WAT = sum(rad_WAT)/len(rad_WAT)
+rl_WAT = ana_WAT.getSummed("length")
+rad_J_WT = ana_J_WT.getParameter("radius")
+rad_J_WT = sum(rad_J_WT)/len(rad_J_WT)
+rl_J_WT = ana_J_WT.getSummed("length")
+rad_J_RMC = ana_J_RMC.getParameter("radius")
+rad_J_RMC = sum(rad_J_RMC)/len(rad_J_RMC)
+rl_J_RMC = ana_J_RMC.getSummed("length")    
+
+print("\n")
+
+print("Simlated average root radius and total root length after", simtime, "days:", "\n")
+print("WurzelAtlas Paras Tomato:", "\n","  Average root radius (cm):", rad_WAT, "\n","  Total root length (cm):", rl_WAT, "\n")
+print("Johanna Wild Type Tomato:", "\n","  Average root radius (cm):", rad_J_WT, "\n","  Total root length (cm):", rl_J_WT, "\n")
+print("Johanna RMC Tomato:", "\n","  Average root radius (cm):", rad_J_RMC, "\n","  Total root length (cm):", rl_J_RMC, "\n")  
+
+
+ana_WAT.write(name_new + ".vtp", ["radius", "subType", "creationTime","organType"])
+ana_J_WT.write(name_Johanna_WildType + ".vtp", ["radius", "subType", "creationTime","organType"])
+ana_J_RMC.write(name_Johanna_RMC + ".vtp", ["radius", "subType", "creationTime","organType"])
+# ana = pb.SegmentAnalyser(mycp)
+# rad = ana.getParameter("radius")
+# rad = sum(rad)/len(rad)
+# rl = ana.getSummed("length")
+# print("\n")
+# print("Simulated Root Atlas Type:")
+# print("Root length total (cm):", rl)
+# # print("Difference to expected lengths (cm):", rl - WT[2])
+# # print("Standard deviation of expected lengths (cm):", WT[3])
+# print("Average root radius (cm):", rad)
+# # print("Difference to expected radius (cm):", rad - WT[0])
+# # print("Standard deviation of expected radius (cm):", WT[1])
+
+# vp.plot_roots(mycp)
+# ana.write(name + ".vtp", ["radius", "subType", "creationTime","organType"])
+# # vp.write_container(pot, "pot.vtp")
+
--- experimental/myc/tomatorootparas.py
+++ experimental/myc/tomatorootparas.py
@@ -10,21 +10,16 @@ import numpy as np
 treatment = 1
 animation = False
 
-treatments = ["WS", "W", "RMCS", "RMC"]
+treatments = ["W","RMC"]
 
-names = ["TomatoJohanna_WildType_Silicone", "TomatoJohanna_WildType", "TomatoJohanna_RMC_Silicone", "TomatoJohanna_RMC"]
+names = ["TomatoJohanna_WildType", "TomatoJohanna_RMC"]
 
 # total root lengths for each sample in mm after 108 days T5 to Q9 in Excel sheet under "TotalRootLength"
-WSLengths = [76145.88048, 133110.4718, 96806.52112, 94722.62373, 60687.46504]
 WLengths = [60645.95887, 70667.46043, 58991.74297, 88779.9162, 86080.82346]
-RMCSLengths = [125165.1072, 134637.2097, 160151.4099, 143844.2341, 151240.8598]
 RMCLengths = [97828.20896, 99849.61423, 108539.5156, 113054.6591, 101631.7984]
 
 # average diameter per sample in mm after 108 days Q4 to T8 in Excel sheet under "Average diameter"
-
-WSDia = [0.313235545, 0.294747188, 0.358293929, 0.351905364, 0.329237833]
 WDia = [0.339339727, 0.342339, 0.342627182, 0.336897846, 0.324256143]
-RMCSDia = [0.36291875, 0.295492267, 0.332729188, 0.313729263, 0.321435375]
 RMCDia = [0.277527526, 0.352695111, 0.276951933, 0.295755846, 0.274213867]
 
 expWLength = sum(WLengths)/len(WLengths)/10 # cm
@@ -49,9 +44,9 @@ WT = [expWRadius, expWRadiusSD, expWLength, expWLengthSD]
 RMC = [expRMCRadius, expRMCRadiusSD, expRMCLength, expRMCLengthSD]
 
 print("Expected values (mean radius cm, radius SD cm, mean length cm, length SD cm):")
-if treatment == 1:
+if treatment == 0:
     print("Wild Type:", WT)
-if treatment == 3:
+if treatment == 1:
     print("RMC:", RMC)
 mycp = pb.MycorrhizalPlant()
 
@@ -93,7 +88,7 @@ rad = ana.getParameter("radius")
 rad = sum(rad)/len(rad)
 rl = ana.getSummed("length")
 print("\n")
-if treatment == 1:
+if treatment == 0:
     print("Simulated Wild Type:")
     print("Root length total (cm):", rl)
     print("Difference to expected lengths (cm):", rl - WT[2])
@@ -102,7 +97,7 @@ if treatment == 1:
     print("Difference to expected radius (cm):", rad - WT[0])
     print("Standard deviation of expected radius (cm):", WT[1])
 
-if treatment == 3:
+if treatment == 1:
     print("Simulated AMF Resistant Type:")
     print("Root length total (cm):", rl)
     print("Difference to expected lengths (cm):", rl - RMC[2])
