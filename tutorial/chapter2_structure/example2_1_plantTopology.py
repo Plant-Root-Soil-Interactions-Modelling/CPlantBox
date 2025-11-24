72e12ed69995a19aafee074b962fe2f684fdc188
m-giraud <71639737+m-giraud@users.noreply.github.com>
2025-11-24 11:25:24 +0100

    add labels

--- tutorial/chapter2_structure/example2_1_plantTopology.py
+++ tutorial/chapter2_structure/example2_1_plantTopology.py
@@ -23,11 +23,11 @@ srp.successorP = [[1.0]]
 srp.successorNo = [1]
 rrp.successorWhere = [[]]  # |\label{l2_1:arrayEnd1}|
 
-p.initialize(False)
+p.initialize(False) # |\label{l2_1:simulateStart1}|
 time = 100
 p.simulate(time, False)
-vp.plot_plant(p, "organType")
-p.write("results/example2_1_2a.vtp")
+vp.plot_plant(p, "organType") 
+p.write("results/example2_1_2a.vtp")  # |\label{l2_1:simulateEnd1}|
 
 # Several successor types, specific locations
 p = pb.MappedPlant(2)
@@ -38,20 +38,20 @@ rrp.successorOT = [[pb.root], [pb.root]]
 rrp.successorST = [[2], [3]]
 rrp.successorNo = [1, 20]
 rrp.successorP = [[1.0], [1.0]]
-rrp.successorWhere = [[-1, -2, -3, -4, -5, -7], [7]]
+rrp.successorWhere = [[-1, -2, -3, -4, -5, -7], [7]] # |\label{l2_1:arrayEndroot2}|
 
 srp = p.getOrganRandomParameter(pb.stem)[1]
 srp.successorOT = [[pb.root], [pb.leaf]]
 srp.successorST = [[4], [1]]
 srp.successorNo = [1, 4]
 srp.successorP = [[1.0], [1.0]]
-srp.successorWhere = [[0.0], [-0.0]]  # |\label{l2_1:arrayEnd2}|
+srp.successorWhere = [[0.0], [-0.0]]  # |\label{l2_1:arrayEndstem2}|
 
-p.initialize(False)
+p.initialize(False)  
 time = 100
 p.simulate(time, False)
 vp.plot_plant(p, "organType")
-p.write("results/example2_1_2b.vtp")
+p.write("results/example2_1_2b.vtp") 
 
 # Probabilistic branching
 p = pb.MappedPlant(2)
@@ -60,14 +60,14 @@ p.readParameters(path + "example2_1_2.xml")
 rrp = p.getOrganRandomParameter(pb.root)[1]  # |\label{l2_1:arrayStart3}|
 rrp.successorOT = [[pb.root]]
 rrp.successorST = [[2]]
-rrp.successorP = [[0.7]]
+rrp.successorP = [[0.7]]  # |\label{l2_1:probabilityroot3}|
 rrp.successorNo = [1]
 rrp.successorWhere = []
 
 srp = p.getOrganRandomParameter(pb.stem)[1]
 srp.successorOT = [[pb.stem, pb.leaf], [pb.stem]]
 srp.successorST = [[2, 1], [2]]
-srp.successorP = [[0.2, 0.8], [1.0]]
+srp.successorP = [[0.2, 0.8], [1.0]]  # |\label{l2_1:probabilitystem3}|
 srp.successorNo = [4, 10]
 srp.successorWhere = [[-3], [3]]  # |\label{l2_1:arrayEnd3}|
 
@@ -76,3 +76,4 @@ time = 100
 p.simulate(time, False)
 vp.plot_plant(p, "organType")
 p.write("results/example2_1_2c.vtp")
+
