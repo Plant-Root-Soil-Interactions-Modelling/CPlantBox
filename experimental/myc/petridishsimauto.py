daf9f9f4499aea915890db6b5d4072813d8ca06a
Anna Heck <an.heck@fz-juelich.de>
2026-03-15 12:42:35 +0100

    made file for petridish simulations

---
+++ experimental/myc/petridishsimauto.py
@@ -0,0 +1,28 @@
+import sys; sys.path.append("../.."); sys.path.append("../../src/")
+
+import plantbox as pb
+import plantbox.visualisation.vtk_plot as vp
+import numpy as np
+
+# automate the petri dish simulation for many petri dishes
+
+# load the tomato grown in petri dish
+
+# check if still need to load the petri dish geometry
+
+# vary seed so different growth patterns emerge
+
+# set different parameters if needed
+
+# run simulation for each seed and save the results in a folder with the seed name
+
+# analyse the results
+
+# check to see if growth follows travelling wave pattern
+
+# set point of growth start
+
+# set some rings similar to corentin
+
+# check for correlation between growth and distance to the rings
+
