eda444b248e447157bcab721c32139cd2a911e90
Anna Heck <an.heck@fz-juelich.de>
2026-03-15 12:45:47 +0100

    mad file to replicate setup henri

---
+++ experimental/myc/setuphenri.py
@@ -0,0 +1,17 @@
+import sys; sys.path.append("../.."); sys.path.append("../../src/")
+
+import plantbox as pb
+import plantbox.visualisation.vtk_plot as vp
+import numpy as np
+
+# set plant
+
+# set geomtry of pot check Henri´s pot sizes
+
+# set parameters for roots and hyphae
+
+# run simulation and save results
+
+# extract soil cores
+
+# analsyse soil cores for hyphal density
