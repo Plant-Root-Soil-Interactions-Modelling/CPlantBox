7fc735d27eeb38db38fb755a5f8f0604430545d5
Daniel Leitner <d.leitner@fz-juelich.de>
2026-04-16 09:07:34 +0200

    exception removed

--- src/structural/organparameter.cpp
+++ src/structural/organparameter.cpp
@@ -91,7 +91,6 @@ double OrganRandomParameter::getParameter(std::string name) const
     if (dparam.count(name)) { // looking for a double parameter
         return *dparam.at(name);
     }
-	throw std::runtime_error("OrganRandomParameter::getParameter: parameter "+name+" not found. Send NaN as output.");
     return std::numeric_limits<double>::quiet_NaN(); // default if name is unknown
 }
 
